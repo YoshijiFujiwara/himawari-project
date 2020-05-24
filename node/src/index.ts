3db600998715ee388e50a210af5317d32dfdf1ab
YoshijiFujiwara <yfbiology2@gmail.com>
2020-05-24 17:51:37 +0900

    codecov test

--- node/src/index.ts
+++ node/src/index.ts
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 import { app } from './app'
 
 const start = async () => {
-  console.log('start!!!!!...fugafugafuga.sss') // test commit
+  console.log('start!!!!!...codecov.sss') // test commit
 
   if (!process.env.MONGO_URI) {
     throw new Error('MONGO_URI が定義されていません')

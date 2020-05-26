c1ba4e95ba069e4f5e0ecf7ba4b09cd2b19d7a49
YoshijiFujiwara <yfbiology2@gmail.com>
2020-05-26 23:40:59 +0900

    setup backend application

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,81 +1,20 @@
 <template>
   <div>
-    <div class="center grid">
-      <vs-row>
-        <vs-col>
-          <form @submit="onSubmit">
-            <vs-input
-              v-model="form.title"
-              placeholder="タイトル入力してエンター"
-            />
-          </form>
-        </vs-col>
-
-        <vs-list>
-          <vs-list-item
-            v-for="(post, i) in posts"
-            :key="i"
-            :title="post.title"
-          ></vs-list-item>
-        </vs-list>
-      </vs-row>
-    </div>
+    <h1>{{title}}</h1>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
-import { Context } from '@nuxt/types'
 
 type DataType = {
-  form: {
-    title: string
-  }
-  posts: Array<{
-    title: string
-  }>
+  title: string
 }
-
 export default Vue.extend({
-  async asyncData({ app }: Context) {
-    const { data } = await app.$axios.get('/api/node/posts')
-    return {
-      posts: data
-    }
-  },
   data(): DataType {
     return {
-      form: {
-        title: ''
-      },
-      posts: []
-    }
-  },
-  methods: {
-    async onSubmit(e: any) {
-      e.preventDefault()
-      const res = await this.$axios.$post('/api/node/posts', {
-        title: this.form.title
-      })
-
-      this.posts = [...this.posts, { title: res.title }]
-      this.form.title = ''
-    },
-    onReset(e: any) {
-      e.preventDefault()
-      this.form.title = ''
+      title: 'hello world'
     }
   }
 })
 </script>
-
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-</style>
---
+++ client/pages/tasks/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>{{title}}</div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  data() {
+    return {
+      title: 'tasks'
+    }
+  }
+})
+</script>

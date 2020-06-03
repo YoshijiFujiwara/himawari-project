710a7b28e8c63a5f16514347a9b92a29351a0a9a
orphis <s_yufuyufu@icloud.com>
2020-06-03 10:37:03 +0900

    空白を消した

--- client/pages/profile.vue
+++ client/pages/profile.vue
@@ -1,14 +1,9 @@
 <template>
     <div>
         <h1>プロフィール</h1>
-
     </div>
 </template>
-
     <script lang="ts">
         import Vue from 'vue'
-
-export default Vue.extend({
-
-        })
+export default Vue.extend({})
 </script>

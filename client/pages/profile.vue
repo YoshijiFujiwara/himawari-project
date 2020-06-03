8e01d4707118ad3327dc31470d6dade324ba5ed1
orphis <s_yufuyufu@icloud.com>
2020-06-03 12:03:35 +0900

    こんとろーるＳして成形された

--- client/pages/profile.vue
+++ client/pages/profile.vue
@@ -1,9 +1,9 @@
 <template>
-    <div>
-        <h1>プロフィール</h1>
-    </div>
+  <div>
+    <h1>プロフィール</h1>
+  </div>
 </template>
-    <script lang="ts">
-        import Vue from 'vue'
+<script lang="ts">
+import Vue from 'vue'
 export default Vue.extend({})
 </script>

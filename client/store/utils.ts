1136710cf497b9340ea4b2d490cbc5f6b916fad5
YoshijiFujiwara <yfbiology2@gmail.com>
2020-06-04 00:38:25 +0900

    ローカル開発でのdocker-composeファイル作成

--- client/store/utils.ts
+++ client/store/utils.ts
@@ -2,8 +2,11 @@ import { BaseAPI } from '~/openapi/base'
 import { Configuration } from '~/openapi'
 
 export function buildApi<T extends BaseAPI>(Api: new (data: any) => T): T {
+  const basePath =
+    process.env.apiUrl ||
+    `${window.location.protocol}//${window.location.hostname}`
   const config = new Configuration({
-    basePath: `${window.location.protocol}//${window.location.hostname}`
+    basePath
   })
   return new Api(config)
 }

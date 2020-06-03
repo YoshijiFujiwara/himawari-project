f086d1bc46ea491a3074de0461867111f70e114b
tk-ozawa <ozawa.shotaro0114@gmail.com>
2020-06-03 22:44:41 +0900

    update: createUser()でパスワードをbcryptでhash化するようにした

--- api/src/auth/user.repository.ts
+++ api/src/auth/user.repository.ts
@@ -1,16 +1,17 @@
 import { EntityRepository, Repository } from 'typeorm';
 import { UserEntity } from './user.entity';
 import { CreateUserDto } from './dto/create-user.dto';
+import * as bcrypt from 'bcrypt';
 
 @EntityRepository(UserEntity)
 export class UserRepository extends Repository<UserEntity> {
   async createUser(createUserDto: CreateUserDto): Promise<UserEntity> {
     const { username, email, password } = createUserDto;
-
+    const hashedPassword = await bcrypt.hash(password, 10);
     const user = new UserEntity();
     user.username = username;
     user.email = email;
-    user.password = password;
+    user.password = hashedPassword;
     await user.save();
 
     return user;

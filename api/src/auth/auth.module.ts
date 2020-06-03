220ede542ab5cb8191ac5ad1820acd72889d19b4
tk-ozawa <ozawa.shotaro0114@gmail.com>
2020-06-03 12:14:02 +0900

    update: authModuleにAuthRepositoryをimport

--- api/src/auth/auth.module.ts
+++ api/src/auth/auth.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthRepository } from './auth.repository';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([AuthRepository])],
   controllers: [AuthController],
   providers: [AuthService],
 })
-export class AuthModule {}
+export class AuthModule { }

d5e9135e38e5d180224ead8dbef076267ae7848a
tk-ozawa <ozawa.shotaro0114@gmail.com>
2020-06-05 15:00:00 +0900

    add: install jwt passport

--- api/src/auth/auth.module.ts
+++ api/src/auth/auth.module.ts
@@ -3,10 +3,17 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserRepository } from './user.repository';
+import { JwtModule } from "@nestjs/jwt"
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserRepository])],
+  imports: [
+    JwtModule.register({
+      secret: 'secret',  // 本番ではランダム文字列に
+      signOptions: { expiresIn: '1h' }
+    }),
+    TypeOrmModule.forFeature([UserRepository])
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
-export class AuthModule {}
+export class AuthModule { }

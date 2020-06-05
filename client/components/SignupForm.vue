dcd2f1a3e1fe19bef756290e6a46bf415eae5425
YoshijiFujiwara <yfbiology2@gmail.com>
2020-06-05 11:02:32 +0900

    making signup form

---
+++ client/components/SignupForm.vue
@@ -0,0 +1,20 @@
+<template>
+  <vs-row vs-align="center" vs-w="8">
+    <h2>Projectに登録</h2>
+    <vs-col vs-type="flex" vs-justify="center" vs-w="12">
+      <vs-input />
+    </vs-col>
+    <vs-col vs-type="flex" vs-justify="center" vs-w="12">
+      <vs-input />
+    </vs-col>
+    <vs-col vs-type="flex" vs-justify="center" vs-w="12">
+      <vs-input />
+    </vs-col>
+  </vs-row>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({})
+</script>

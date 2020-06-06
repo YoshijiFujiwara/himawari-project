775a1e00656e92a651d02859fb1033b627f0e51c
YoshijiFujiwara <yfbiology2@gmail.com>
2020-06-06 23:45:41 +0900

    adding task controller spec

---
+++ api/src/tasks/tasks.controller.spec.ts
@@ -0,0 +1,122 @@
+import { TasksController } from './tasks.controller';
+import { TasksService } from './tasks.service';
+import { TaskSerializer } from './task.serializer';
+import { TaskEntity } from './task.entity';
+import { TaskStatus } from './task-status.enum';
+import { TaskRepository } from './task.repository';
+import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
+import { CreateTaskDto } from './dto/create-task.dto';
+
+const buildTask = ({
+  id,
+  title,
+  description,
+  status,
+}: Pick<TaskEntity, 'id' | 'title' | 'description' | 'status'>): TaskEntity => {
+  const task = new TaskEntity();
+  task.id = id;
+  task.title = title;
+  task.description = description;
+  task.status = status;
+  return task;
+};
+
+describe('tasksController', () => {
+  let tasksController: TasksController;
+  let tasksService: TasksService;
+  beforeEach(() => {
+    // tslint:disable-next-line:prefer-const
+    let taskRepository: TaskRepository;
+    tasksService = new TasksService(taskRepository);
+    tasksController = new TasksController(tasksService);
+  });
+
+  describe('findAll', () => {
+    it('タスクの一覧を返却する', async () => {
+      const result = [
+        buildTask({
+          id: 1,
+          title: 'specialtitle',
+          description: 'fuga',
+          status: TaskStatus.OPEN,
+        }),
+        buildTask({
+          id: 2,
+          title: 'hoge',
+          description: 'hogehoge',
+          status: TaskStatus.IN_PROGRESS,
+        }),
+      ];
+      jest
+        .spyOn(tasksService, 'getTasks')
+        .mockImplementation(() => Promise.resolve(result));
+
+      const filterDto: GetTasksFilterDto = { status: null, search: null };
+      expect(await tasksController.getTasks(filterDto)).toBe(result);
+    });
+  });
+
+  describe('getTaskById', () => {
+    it('idで指定したタスクを1件返す', async () => {
+      const result = buildTask({
+        id: 1,
+        title: 'specialtitle',
+        description: 'fuga',
+        status: TaskStatus.OPEN,
+      });
+      jest
+        .spyOn(tasksService, 'getTaskById')
+        .mockImplementation(() => Promise.resolve(result));
+
+      expect(await tasksController.getTaskById(1)).toBe(result);
+    });
+  });
+
+  describe('createTask', () => {
+    it('タスクを1件作成する', async () => {
+      const result = buildTask({
+        id: 1,
+        title: 'specialtitle',
+        description: 'fuga',
+        status: TaskStatus.OPEN,
+      });
+      jest
+        .spyOn(tasksService, 'createTask')
+        .mockImplementation(() => Promise.resolve(result));
+
+      const createTaskDto: CreateTaskDto = {
+        title: 'fuga',
+        description: 'hoge',
+      };
+      expect(await tasksController.createTask(createTaskDto)).toBe(result);
+    });
+  });
+
+  describe('deleteTask', () => {
+    it('タスクを1件削除する', async () => {
+      jest
+        .spyOn(tasksService, 'deleteTask')
+        .mockImplementation(() => Promise.resolve());
+
+      expect(await tasksController.deleteTask(1)).toBe(undefined);
+    });
+  });
+
+  describe('updateTask', () => {
+    it('タスクのステータスを更新する', async () => {
+      const result = buildTask({
+        id: 1,
+        title: 'specialtitle',
+        description: 'fuga',
+        status: TaskStatus.OPEN,
+      });
+      jest
+        .spyOn(tasksService, 'updateTaskStatus')
+        .mockImplementation(() => Promise.resolve(result));
+
+      expect(await tasksController.updateTaskStatus(1, TaskStatus.OPEN)).toBe(
+        result,
+      );
+    });
+  });
+});

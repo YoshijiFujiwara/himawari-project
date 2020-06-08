5bfe3f23f691f178f59276b53ad41262cd762709
tk-ozawa <ozawa.shotaro0114@gmail.com>
2020-06-08 15:56:22 +0900

    update: authServiceのsignIn()の各返り値の型を指定

--- api/src/auth/auth.service.ts
+++ api/src/auth/auth.service.ts
@@ -4,6 +4,8 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { UserRepository } from './user.repository';
 import { JwtService } from '@nestjs/jwt';
 import { SingInUserDto } from './dto/sign-in-user.dto';
+import { JwtPayload } from './interface/jwt-payload.interface';
+import { AccessToken } from './interface/access-token.interface';
 
 @Injectable()
 export class AuthService {
@@ -17,8 +19,10 @@ export class AuthService {
     return this.userRepository.createUser(signUpUserDto);
   }
 
-  async signIn(signInUserDto: SingInUserDto): Promise<{ accessToken: string }> {
-    const payload = await this.userRepository.validatePassword(signInUserDto);
+  async signIn(signInUserDto: SingInUserDto): Promise<AccessToken> {
+    const payload: JwtPayload = await this.userRepository.validatePassword(
+      signInUserDto,
+    );
 
     return {
       accessToken: this.jwtService.sign(payload),

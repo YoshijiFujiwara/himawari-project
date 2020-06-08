7238aecf6b19257204507ff70f3b3cb64db7c84f
Sui1224 <sui.ohs75003@gmail.com>
2020-06-08 18:17:28 +0900

    バリデーションのモードをeagerに変更

--- client/components/SignupForm.vue
+++ client/components/SignupForm.vue
@@ -2,52 +2,72 @@
   <vs-row vs-align="center" vs-w="8">
     <h2 class="form-title">Projectに登録</h2>
     <div class="input-container">
-      <vs-col>
-        <validation-provider
-          v-slot="{ errors }"
-          rules="required"
-          name="ユーザー名"
-        >
-          <vs-input v-model="form.username" label="ユーザ名" />
-          <span v-show="errors.length" class="help is-danger">
-            {{ errors[0] }}
-          </span>
-        </validation-provider>
-      </vs-col>
-      <vs-col>
-        <validation-provider
-          v-slot="{ errors }"
-          rules="required|email"
-          name="メールアドレス"
-        >
-          <vs-input v-model="form.email" label="メールアドレス" />
-          <span v-show="errors.length" class="help is-danger">
-            {{ errors[0] }}
-          </span>
-        </validation-provider>
-      </vs-col>
-      <vs-col>
-        <validation-provider
-          v-slot="{ errors }"
-          rules="required|min:6|max:20"
-          name="パスワード"
-        >
-          <vs-input v-model="form.password" label="パスワード" />
-          <span v-show="errors.length" class="help is-danger">
-            {{ errors[0] }}
-          </span>
-        </validation-provider>
-      </vs-col>
-      <vs-col vs-type="flex" vs-justify="start" vs-w="12">
-        <vs-checkbox v-model="form.isAgreed" icon="✓"
-          >利用規約に同意します</vs-checkbox
-        >
-      </vs-col>
-      <vs-col vs-type="flex" vs-justify="start" vs-w="12">
-        <vs-button color="primary" type="filled" @click="onSubmit"
-          >アカウントを作成する
-        </vs-button>
-      </vs-col>
+      <validation-observer
+        ref="observer"
+        v-slot="{ invalid }"
+        tag="form"
+        @submit.prevent="onSubmit()"
+      >
+        <vs-col>
+          <validation-provider
+            v-slot="{ errors }"
+            rules="required|min:5|max:20"
+            name="ユーザー名"
+            mode="eager"
+          >
+            <vs-input v-model="form.username" label="ユーザ名" />
+            <span v-show="errors.length" class="help is-danger">
+              {{ errors[0] }}
+            </span>
+          </validation-provider>
+        </vs-col>
+        <vs-col>
+          <validation-provider
+            v-slot="{ errors }"
+            rules="required|email"
+            name="メールアドレス"
+            mode="eager"
+          >
+            <vs-input v-model="form.email" label="メールアドレス" />
+            <span v-show="errors.length" class="help is-danger">
+              {{ errors[0] }}
+            </span>
+          </validation-provider>
+        </vs-col>
+        <vs-col>
+          <validation-provider
+            v-slot="{ errors }"
+            rules="required|min:6|max:20"
+            name="パスワード"
+            mode="eager"
+          >
+            <vs-input v-model="form.password" label="パスワード" />
+            <span v-show="errors.length" class="help is-danger">
+              {{ errors[0] }}
+            </span>
+          </validation-provider>
+        </vs-col>
+        <vs-col vs-type="flex" vs-justify="start" vs-w="12">
+          <validation-provider
+            v-slot="{ errors }"
+            rules="required:true"
+            name="同意"
+            mode="eager"
+          >
+            <vs-checkbox v-model="form.isAgreed" icon="✓"
+              >利用規約に同意します</vs-checkbox
+            >
+            <span v-show="errors.length" class="help is-danger">
+              {{ errors[0] }}
+            </span>
+          </validation-provider>
+        </vs-col>
+        <vs-col vs-type="flex" vs-justify="start" vs-w="12">
+          <vs-button color="primary" type="filled" :disabled="invalid"
+            >アカウントを作成する
+          </vs-button>
+        </vs-col>
+      </validation-observer>
     </div>
   </vs-row>
 </template>

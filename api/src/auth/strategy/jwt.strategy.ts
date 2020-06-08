2f093e30e598a10443a77e058dc2c7a99c425bc2
tk-ozawa <ozawa.shotaro0114@gmail.com>
2020-06-08 22:29:21 +0900

    update: JwtStrategyにてpayloadからuserEntityを取得するようにした

--- api/src/auth/strategy/jwt.strategy.ts
+++ api/src/auth/strategy/jwt.strategy.ts
@@ -1,11 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { Strategy, ExtractJwt } from 'passport-jwt';
 import { JwtPayload } from '../interface/jwt-payload.interface';
+import { UserRepository } from '../user.repository';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor() {
+  constructor(
+    @InjectRepository(UserRepository)
+    private userRepository: UserRepository,
+  ) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
@@ -14,6 +19,11 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   }
 
   async validate(payload: JwtPayload) {
-    return payload;
+    const { username } = payload;
+    const user = await this.userRepository.findOne({ username });
+    if (!user) {
+      throw new UnauthorizedException();
+    }
+    return user;
   }
 }

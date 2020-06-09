a05c0cd192f3c75e2d42943acf81736dfa161d2d
YoshijiFujiwara <yfbiology2@gmail.com>
2020-06-09 17:33:13 +0900

    display: none設定忘れ

--- client/components/Loading.vue
+++ client/components/Loading.vue
@@ -2,5 +2,5 @@
   <!-- loadingのcomputedプロパティを強制的に活性化するため。表示はしない -->
   <!-- mixin経由しないと、[vuex] do not mutate vuex store state outside mutation handlers. -->
   <!-- が出るためそうしています -->
-  <div>{{ globalLoading }}</div>
+  <div style="display: none;">{{ globalLoading }}</div>
 </template>

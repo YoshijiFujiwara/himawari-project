b5ef842fc05b980b796f6f9a6a9077948de33c5e
YoshijiFujiwara <yfbiology2@gmail.com>
2020-06-10 13:27:51 +0900

    Inputのエラーメッセージを表示する、InputErrorコンポーネントを作成

---
+++ client/components/InputError.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <span v-if="errors.length" class="help is-danger">
+      {{ errors[0] }}
+    </span>
+    <span v-else class="help is-clear"><br /></span>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue, { PropType } from 'vue'
+export default Vue.extend({
+  props: {
+    errors: {
+      type: Array as PropType<string[]>,
+      default: []
+    }
+  }
+})
+</script>
+
+<style lang="scss">
+.is-danger {
+  display: block;
+  padding-top: 3px;
+  color: #fa0000;
+}
+.is-clear {
+  display: block;
+  padding-top: 3px;
+  color: #ffffff;
+}
+</style>

7db0bd9c57a6d052b1c3ac18ac5a340396543adb
kooririri <piaoxianglunb@gmail.com>
2020-06-10 10:52:14 +0900

    adding signin form

---
+++ client/components/SigninForm.vue
@@ -0,0 +1,147 @@
+<template>
+  <vs-row vs-align="center" vs-w="8">
+    <h2 class="form-title">Projectにログイン</h2>
+    <div class="input-container">
+      <validation-observer ref="observer" v-slot="{ invalid }" tag="form">
+        <vs-col>
+          <validation-provider
+            v-slot="{ errors }"
+            rules="required|min:5|max:20"
+            name="ユーザー名"
+          >
+            <vs-input v-model="form.username" label="ユーザ名" />
+            <span v-show="errors.length" class="help is-danger">
+              {{ errors[0] }}
+            </span>
+          </validation-provider>
+        </vs-col>
+        <vs-col>
+          <validation-provider
+            v-slot="{ errors }"
+            rules="required|min:6|max:20"
+            name="パスワード"
+          >
+            <vs-input v-model="form.password" label="パスワード" />
+            <span v-show="errors.length" class="help is-danger">
+              {{ errors[0] }}
+            </span>
+          </validation-provider>
+        </vs-col>
+        <vs-col>
+          <p>
+            パスワードをお忘れの方は<nuxt-link to="/users/signin"
+              >こちら</nuxt-link
+            >
+          </p>
+        </vs-col>
+        <vs-col vs-type="flex" vs-justify="center" vs-align="center" vs-w="12">
+          <vs-button
+            color="primary"
+            type="filled"
+            vs-w="12"
+            :disabled="invalid"
+            @click="onSubmit"
+            >ログイン
+          </vs-button>
+        </vs-col>
+        <vs-col vs-type="flex" vs-justify="center" vs-align="center" vs-w="12">
+          <vs-col vs-type="flex" vs-justify="center" vs-align="center" vs-w="5">
+            <hr class="hr-5" />
+          </vs-col>
+          <vs-col vs-type="flex" vs-justify="center" vs-align="center" vs-w="2">
+            または
+          </vs-col>
+          <vs-col vs-type="flex" vs-justify="center" vs-align="center" vs-w="5">
+            <hr class="hr-5" />
+          </vs-col>
+        </vs-col>
+        <vs-col vs-type="flex" vs-justify="center" vs-align="center" vs-w="12">
+          <vs-button
+            class="btn-google"
+            color="primary"
+            type="filled"
+            vs-w="12"
+            @click="onClickGoogleButton"
+          >
+            Googleアカウントでログイン
+          </vs-button>
+        </vs-col>
+      </validation-observer>
+    </div>
+  </vs-row>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { authStore } from '@/store/modules/auth'
+
+type Data = {
+  form: {
+    username: string
+    email: string
+    password: string
+    isAgreed: string | undefined
+  }
+}
+export default Vue.extend({
+  data(): Data {
+    return {
+      form: {
+        username: '',
+        email: '',
+        password: '',
+        isAgreed: undefined
+      }
+    }
+  },
+  methods: {
+    async onSubmit() {
+      const result = await authStore.signup(this.form)
+      if (result) {
+        this.form.username = ''
+        this.form.email = ''
+        this.form.password = ''
+
+        this.$router.push('/mailsend')
+      }
+    },
+    onClickGoogleButton() {
+      alert('google')
+    }
+  }
+})
+</script>
+
+<style lang="scss">
+.form-title {
+  font-size: 36px;
+  color: #707070;
+}
+.input-container {
+  .vs-col {
+    margin-bottom: 20px;
+    color: #777777;
+    font-family: HiraginoSans-W5;
+    .vs-input {
+      width: 100%;
+    }
+    .btn-google {
+      margin-top: 20px;
+      width: 100%;
+    }
+    .hr-5 {
+      width: 95%;
+      vertical-align: middle;
+      border: solid 1px #777777;
+    }
+    .vs-button-text {
+      align-content: center;
+    }
+  }
+}
+.is-danger {
+  display: block;
+  padding-top: 3px;
+  color: #fa0000;
+}
+</style>

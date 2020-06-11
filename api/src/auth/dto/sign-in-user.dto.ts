dd3c7327a9b6bf37917da71afacf033f707eaf67
tk-ozawa <ozawa.shotaro0114@gmail.com>
2020-06-11 15:03:08 +0900

    chore: linting

--- api/src/auth/dto/sign-in-user.dto.ts
+++ api/src/auth/dto/sign-in-user.dto.ts
@@ -1,5 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsString, IsOptional } from 'class-validator';
+import { IsString } from 'class-validator';
 
 export class SignInUserDto {
   // ユーザー名とメールアドレスはsign_in時はOR

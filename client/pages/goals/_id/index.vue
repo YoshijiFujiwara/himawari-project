55b79199beb76dcf4c78e3e7698ea904be753ddc
orphis <s_yufuyufu@icloud.com>
2020-06-14 21:56:32 +0900

    アイコンわからん

--- client/pages/goals/_id/index.vue
+++ client/pages/goals/_id/index.vue
@@ -1,94 +1,103 @@
 <template>
   <div>
     <div class="wrapper">
-      <vs-card class="cardx" fixed-height>
-        <div>
-          <vs-list>
-            <vs-list-item title="yyyy年mm月dd日"></vs-list-item>
-            <vs-collapse accordion>
-              <vs-collapse-item>
-                <div slot="header">
-                  <p class="small-goal">小目標</p>
-                  <span class="study-time">学習時間</span>
-                </div>
-                1
-                <br /><br />
-              </vs-collapse-item>
-              <vs-collapse-item>
-                <div slot="header">
-                  <p class="small-goal">小目標</p>
-                  <span class="study-time">学習時間</span>
-                </div>
-                2
-              </vs-collapse-item>
-              <vs-collapse-item>
-                <div slot="header">
-                  <p class="small-goal">小目標</p>
-                  <span class="study-time">学習時間</span>
-                </div>
-                3
-              </vs-collapse-item>
-              <vs-collapse-item>
-                <div slot="header">
-                  <p class="small-goal">小目標</p>
-                  <span class="study-time">学習時間</span>
-                </div>
-                4
-              </vs-collapse-item>
-              <vs-collapse-item>
-                <div slot="header">
-                  <p class="small-goal">小目標</p>
-                  <span class="study-time">学習時間</span>
-                </div>
-                5
-              </vs-collapse-item>
-            </vs-collapse>
-            <vs-list-item title="yyyy年mm月dd日"></vs-list-item>
-            <vs-collapse accordion>
-              <vs-collapse-item>
-                <div slot="header">
-                  <p class="small-goal">小目標</p>
-                  <span class="study-time">学習時間</span>
-                </div>
-                1
-                <br /><br />
-              </vs-collapse-item>
-              <vs-collapse-item>
-                <div slot="header">
-                  <p class="small-goal">小目標</p>
-                  <span class="study-time">学習時間</span>
-                </div>
-                2
-              </vs-collapse-item>
-            </vs-collapse>
-            <vs-list-item title="yyyy年mm月dd日"></vs-list-item>
-            <vs-collapse accordion>
-              <vs-collapse-item>
-                <div slot="header">
-                  <p class="small-goal">小目標</p>
-                  <span class="study-time">学習時間</span>
-                </div>
-                1
-                <br /><br />
-              </vs-collapse-item>
-              <vs-collapse-item>
-                <div slot="header">
-                  <p class="small-goal">小目標</p>
-                  <span class="study-time">学習時間</span>
-                </div>
-                2
-              </vs-collapse-item>
-              <vs-collapse-item>
-                <div slot="header">
-                  <p class="small-goal">小目標</p>
-                  <span class="study-time">学習時間</span>
-                </div>
-                3
-              </vs-collapse-item>
-            </vs-collapse>
-          </vs-list>
-        </div>
-      </vs-card>
+      <div class="content">
+        <h1>ユーザ名｜TOEICで800点以上</h1>
+        <p>
+          目標作成ページの「目標について」の内容が表示されます。目標作成ページの「目標について」の内容が表示されます。<br />
+          目標作成ページの「目標について」の内容が表示されます。目標作成ページの「目標について」の内容が表示されます。<br />
+          目標作成ページの「目標について」の内容が表示されます。目標作成ページの「目標について」の内容が表示されます。<br />
+        </p>
+        <h2>学習記録</h2>
+        <vs-card class="cardx" fixed-height>
+          <div>
+            <vs-list>
+              <vs-list-item title="yyyy年mm月dd日"></vs-list-item>
+              <vs-collapse accordion>
+                <vs-collapse-item>
+                  <div slot="header">
+                    <p class="small-goal">小目標</p>
+                    <span class="study-time">学習時間</span>
+                  </div>
+                  1
+                  <br /><br />
+                </vs-collapse-item>
+                <vs-collapse-item>
+                  <div slot="header">
+                    <p class="small-goal">小目標</p>
+                    <span class="study-time">学習時間</span>
+                  </div>
+                  2
+                </vs-collapse-item>
+                <vs-collapse-item>
+                  <div slot="header">
+                    <p class="small-goal">小目標</p>
+                    <span class="study-time">学習時間</span>
+                  </div>
+                  3
+                </vs-collapse-item>
+                <vs-collapse-item>
+                  <div slot="header">
+                    <p class="small-goal">小目標</p>
+                    <span class="study-time">学習時間</span>
+                  </div>
+                  4
+                </vs-collapse-item>
+                <vs-collapse-item>
+                  <div slot="header">
+                    <p class="small-goal">小目標</p>
+                    <span class="study-time">学習時間</span>
+                  </div>
+                  5
+                </vs-collapse-item>
+              </vs-collapse>
+              <vs-list-item title="yyyy年mm月dd日"></vs-list-item>
+              <vs-collapse accordion>
+                <vs-collapse-item>
+                  <div slot="header">
+                    <p class="small-goal">小目標</p>
+                    <span class="study-time">学習時間</span>
+                  </div>
+                  1
+                  <br /><br />
+                </vs-collapse-item>
+                <vs-collapse-item>
+                  <div slot="header">
+                    <p class="small-goal">小目標</p>
+                    <span class="study-time">学習時間</span>
+                  </div>
+                  2
+                </vs-collapse-item>
+              </vs-collapse>
+              <vs-list-item title="yyyy年mm月dd日"></vs-list-item>
+              <vs-collapse accordion>
+                <vs-collapse-item>
+                  <div slot="header">
+                    <p class="small-goal">小目標</p>
+                    <span class="study-time">学習時間</span>
+                  </div>
+                  1
+                  <br /><br />
+                </vs-collapse-item>
+                <vs-collapse-item>
+                  <div slot="header">
+                    <p class="small-goal">小目標</p>
+                    <span class="study-time">学習時間</span>
+                  </div>
+                  2
+                </vs-collapse-item>
+                <vs-collapse-item>
+                  <div slot="header">
+                    <p class="small-goal">小目標</p>
+                    <span class="study-time">学習時間</span>
+                  </div>
+                  3
+                </vs-collapse-item>
+              </vs-collapse>
+            </vs-list>
+          </div>
+        </vs-card>
+      </div>
     </div>
   </div>
 </template>
@@ -103,8 +112,11 @@ export default Vue.extend({})
   width: 100%;
   background-color: #eff7ff;
 }
-.cardx {
+.content {
+  margin: auto;
   width: 66%;
+}
+.cardx {
   margin: auto;
 }
 .study-time {

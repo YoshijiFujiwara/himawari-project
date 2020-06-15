c90583e2631afb62ff6d3914b1e87519e3e581b4
Sui1224 <sui.ohs75003@gmail.com>
2020-06-15 14:23:21 +0900

    flexむずすぎエレガントならん

--- client/pages/goals/new.vue
+++ client/pages/goals/new.vue
@@ -1,4 +1,95 @@
-<template> <vs-row> </vs-row> </template>
+<template>
+  <div>
+    <vs-row vs-justify="center">
+      <vs-col type="flex" vs-justify="center" vs-align="center" vs-w="6.6">
+        <vs-card class="cardx">
+          <div slot="header">
+            <vs-row vs-justify="flex-start">
+              <vs-col vs-offset="0.5" vs-type="flex">
+                <vs-icon
+                  icon="flag"
+                  class="main"
+                  color="#54a9fe"
+                  size="medium"
+                ></vs-icon>
+                <h1>
+                  新しい目標を作成する
+                </h1>
+              </vs-col>
+            </vs-row>
+            <vs-row>
+              <vs-col vs-offset="0.5" vs-w="11">
+                <vs-divider></vs-divider>
+              </vs-col>
+            </vs-row>
+            <vs-row class="goal">
+              <vs-col vs-w="5" vs-offset="0.7">
+                <h3>目標<span class="required">*</span></h3>
+                <vs-input />
+              </vs-col>
+            </vs-row>
+            <vs-row class="description">
+              <vs-col vs-w="8" vs-offset="0.7">
+                <h3>目標について</h3>
+                <vs-textarea v-model="textarea" />
+              </vs-col>
+            </vs-row>
+            <vs-row>
+              <vs-col vs-offset="0.5" vs-w="11">
+                <vs-divider></vs-divider>
+              </vs-col>
+            </vs-row>
+            <vs-row class="public">
+              <vs-col vs-w="10" vs-offset="0.8">
+                <vs-radio v-model="radios1" vs-name="radios1" vs-value="true">
+                  <vs-col vs-type="flex" vs-offset="0.2">
+                    <vs-icon
+                      icon="import_contacts"
+                      color="#606060"
+                      size="large"
+                    ></vs-icon>
+                    <vs-col vs-w="10" vs-offset="0.3">
+                      <h3>公開</h3>
+                      <p>
+                        誰でもこの目標を見ることができます。目標を公開にして、みんなと応援し合いましょう！
+                      </p>
+                    </vs-col>
+                  </vs-col>
+                </vs-radio>
+              </vs-col>
+            </vs-row>
+            <vs-row class="public">
+              <vs-col vs-w="10" vs-offset="0.4">
+                <vs-radio v-model="radios1" vs-name="radios1" vs-value="false">
+                  <vs-row>
+                    <vs-col vs-type="flex" vs-offset="0.2">
+                      <vs-icon
+                        icon="enhanced_encryption"
+                        color="#606060"
+                        size="large"
+                      ></vs-icon>
+                      <vs-col vs-w="10" vs-offset="0.3">
+                        <h3>非公開</h3>
+                        <p>
+                          この目標はあなたもしくは、グループメンバーしか見ることができません。
+                        </p>
+                      </vs-col>
+                    </vs-col>
+                  </vs-row>
+                </vs-radio>
+              </vs-col>
+            </vs-row>
+            <vs-row>
+              <vs-col vs-offset="0.5" vs-w="11">
+                <vs-divider></vs-divider>
+              </vs-col>
+            </vs-row>
+          </div>
+        </vs-card>
+      </vs-col>
+    </vs-row>
+  </div>
+</template>
 
 <script lang="ts">
 import Vue from 'vue'
@@ -6,4 +97,33 @@ import Vue from 'vue'
 export default Vue.extend({})
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+h1 {
+  color: #707070;
+  font-size: 36px;
+}
+.description {
+  margin-top: 30px;
+}
+.vs-input {
+  width: 100%;
+}
+.required {
+  color: #d00505;
+}
+.vs-icon {
+  padding-top: 10px;
+}
+.public,
+.goal,
+.description {
+  h3 {
+    color: #707070;
+    font-size: 20px;
+  }
+  p {
+    color: #999999;
+    font-size: 16px;
+  }
+}
+</style>

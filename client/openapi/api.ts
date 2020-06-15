4834818d1ee61eba3cc464386d03f26a26849bc8
tk-ozawa <ozawa.shotaro0114@gmail.com>
2020-06-15 17:44:42 +0900

    chore: codegen

--- client/openapi/api.ts
+++ client/openapi/api.ts
@@ -1,5 +1,4 @@
-/* tslint:disable */
-/* eslint-disable */
+// tslint:disable
 /**
  * ひまわりプロジェクト
  * APIドキュメント
@@ -32,6 +31,31 @@ export interface AccessTokenSerializer {
      * @memberof AccessTokenSerializer
      */
     accessToken: string;
+}
+/**
+ * 
+ * @export
+ * @interface CreateGoalDto
+ */
+export interface CreateGoalDto {
+    /**
+     * 
+     * @type {string}
+     * @memberof CreateGoalDto
+     */
+    title: string;
+    /**
+     * 
+     * @type {string}
+     * @memberof CreateGoalDto
+     */
+    description?: string;
+    /**
+     * publicに公開するか否か
+     * @type {boolean}
+     * @memberof CreateGoalDto
+     */
+    is_public: boolean;
 }
 /**
  * 
@@ -587,6 +611,122 @@ export class AuthApi extends BaseAPI {
      */
     public authControllerVerifyEmail(token: string, options?: any) {
         return AuthApiFp(this.configuration).authControllerVerifyEmail(token, options).then((request) => request(this.axios, this.basePath));
+    }
+}
+
+
+/**
+ * GoalsApi - axios parameter creator
+ * @export
+ */
+export const GoalsApiAxiosParamCreator = function (configuration?: Configuration) {
+    return {
+        /**
+         * 
+         * @param {CreateGoalDto} createGoalDto 
+         * @param {*} [options] Override http request option.
+         * @throws {RequiredError}
+         */
+        goalsControllerCreateGoals: async (createGoalDto: CreateGoalDto, options: any = {}): Promise<RequestArgs> => {
+            // verify required parameter 'createGoalDto' is not null or undefined
+            if (createGoalDto === null || createGoalDto === undefined) {
+                throw new RequiredError('createGoalDto','Required parameter createGoalDto was null or undefined when calling goalsControllerCreateGoals.');
+            }
+            const localVarPath = `/api/goals`;
+            const localVarUrlObj = globalImportUrl.parse(localVarPath, true);
+            let baseOptions;
+            if (configuration) {
+                baseOptions = configuration.baseOptions;
+            }
+            const localVarRequestOptions = { method: 'POST', ...baseOptions, ...options};
+            const localVarHeaderParameter = {} as any;
+            const localVarQueryParameter = {} as any;
+
+            // authentication bearer required
+            // http bearer authentication required
+            if (configuration && configuration.accessToken) {
+                const accessToken = typeof configuration.accessToken === 'function'
+                    ? configuration.accessToken()
+                    : configuration.accessToken;
+                localVarHeaderParameter["Authorization"] = "Bearer " + accessToken;
+            }
+
+
+    
+            localVarHeaderParameter['Content-Type'] = 'application/json';
+
+            localVarUrlObj.query = {...localVarUrlObj.query, ...localVarQueryParameter, ...options.query};
+            // fix override query string Detail: https://stackoverflow.com/a/7517673/1077943
+            delete localVarUrlObj.search;
+            let headersFromBaseOptions = baseOptions && baseOptions.headers ? baseOptions.headers : {};
+            localVarRequestOptions.headers = {...localVarHeaderParameter, ...headersFromBaseOptions, ...options.headers};
+            const needsSerialization = (typeof createGoalDto !== "string") || localVarRequestOptions.headers['Content-Type'] === 'application/json';
+            localVarRequestOptions.data =  needsSerialization ? JSON.stringify(createGoalDto !== undefined ? createGoalDto : {}) : (createGoalDto || "");
+
+            return {
+                url: globalImportUrl.format(localVarUrlObj),
+                options: localVarRequestOptions,
+            };
+        },
+    }
+};
+
+/**
+ * GoalsApi - functional programming interface
+ * @export
+ */
+export const GoalsApiFp = function(configuration?: Configuration) {
+    return {
+        /**
+         * 
+         * @param {CreateGoalDto} createGoalDto 
+         * @param {*} [options] Override http request option.
+         * @throws {RequiredError}
+         */
+        async goalsControllerCreateGoals(createGoalDto: CreateGoalDto, options?: any): Promise<(axios?: AxiosInstance, basePath?: string) => AxiosPromise<void>> {
+            const localVarAxiosArgs = await GoalsApiAxiosParamCreator(configuration).goalsControllerCreateGoals(createGoalDto, options);
+            return (axios: AxiosInstance = globalAxios, basePath: string = BASE_PATH) => {
+                const axiosRequestArgs = {...localVarAxiosArgs.options, url: basePath + localVarAxiosArgs.url};
+                return axios.request(axiosRequestArgs);
+            };
+        },
+    }
+};
+
+/**
+ * GoalsApi - factory interface
+ * @export
+ */
+export const GoalsApiFactory = function (configuration?: Configuration, basePath?: string, axios?: AxiosInstance) {
+    return {
+        /**
+         * 
+         * @param {CreateGoalDto} createGoalDto 
+         * @param {*} [options] Override http request option.
+         * @throws {RequiredError}
+         */
+        goalsControllerCreateGoals(createGoalDto: CreateGoalDto, options?: any): AxiosPromise<void> {
+            return GoalsApiFp(configuration).goalsControllerCreateGoals(createGoalDto, options).then((request) => request(axios, basePath));
+        },
+    };
+};
+
+/**
+ * GoalsApi - object-oriented interface
+ * @export
+ * @class GoalsApi
+ * @extends {BaseAPI}
+ */
+export class GoalsApi extends BaseAPI {
+    /**
+     * 
+     * @param {CreateGoalDto} createGoalDto 
+     * @param {*} [options] Override http request option.
+     * @throws {RequiredError}
+     * @memberof GoalsApi
+     */
+    public goalsControllerCreateGoals(createGoalDto: CreateGoalDto, options?: any) {
+        return GoalsApiFp(this.configuration).goalsControllerCreateGoals(createGoalDto, options).then((request) => request(this.axios, this.basePath));
     }
 }
 

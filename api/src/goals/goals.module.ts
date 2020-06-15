01f9a314ba1cf180acb671d649f4396fa5dbd34e
tk-ozawa <ozawa.shotaro0114@gmail.com>
2020-06-15 14:56:30 +0900

    update: GoalModuleにORMの設定追加

--- api/src/goals/goals.module.ts
+++ api/src/goals/goals.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { GoalsService } from './goals.service';
 import { GoalsController } from './goals.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { GoalRepository } from './goal.repository';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([GoalRepository]), AuthModule],
   providers: [GoalsService],
   controllers: [GoalsController],
 })

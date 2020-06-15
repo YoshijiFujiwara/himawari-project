ad93df80c48eff8b26664863049bc0a14d82a8bb
tk-ozawa <ozawa.shotaro0114@gmail.com>
2020-06-15 14:57:00 +0900

    add: Goal用ダミーAPI作成

--- api/src/goals/goals.controller.ts
+++ api/src/goals/goals.controller.ts
@@ -1,4 +1,27 @@
-import { Controller } from '@nestjs/common';
+import { Controller, UseGuards, Post, Body } from '@nestjs/common';
+import { ApiTags, ApiBearerAuth, ApiResponse } from '@nestjs/swagger';
+import { AuthGuard } from '@nestjs/passport';
+import { GoalsService } from './goals.service';
+import { GetUser } from '../auth/get-user-decorator';
+import { UserEntity } from '../auth/user.entity';
+import { CreateGoalDto } from './dto/create-goal.dto';
 
+@ApiTags('goals')
 @Controller('goals')
-export class GoalsController {}
+@UseGuards(AuthGuard('jwt'))
+@ApiBearerAuth()
+export class GoalsController {
+  constructor(private goalsService: GoalsService) {}
+
+  @Post()
+  @ApiResponse({
+    status: 201,
+    description: '目標の作成',
+  })
+  createGoals(
+    @Body() createGoalDto: CreateGoalDto,
+    @GetUser() user: UserEntity,
+  ) {
+    return this.goalsService.createGoal(createGoalDto, user);
+  }
+}
--- api/src/goals/goals.service.ts
+++ api/src/goals/goals.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { GoalRepository } from './goal.repository';
+import { CreateGoalDto } from './dto/create-goal.dto';
+import { UserEntity } from '../auth/user.entity';
 
 @Injectable()
-export class GoalsService {}
+export class GoalsService {
+  constructor(
+    @InjectRepository(GoalRepository)
+    private goalRepository: GoalRepository,
+  ) {}
+
+  async createGoal(createGoalDto: CreateGoalDto, user: UserEntity) {
+    return this.goalRepository.createGoal(createGoalDto, user);
+  }
+}

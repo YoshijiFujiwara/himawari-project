ec038ec9dee001e3485a841aa316244e751d7266
tk-ozawa <ozawa.shotaro0114@gmail.com>
2020-06-17 10:48:33 +0900

    chore: fix lint

--- api/src/goals/goal.repository.ts
+++ api/src/goals/goal.repository.ts
@@ -1,4 +1,4 @@
-import { EntityRepository, Repository, Brackets } from 'typeorm';
+import { EntityRepository, Repository } from 'typeorm';
 import { GoalEntity } from './goal.entity';
 import { CreateGoalDto } from './dto/create-goal.dto';
 import { UserEntity } from '../auth/user.entity';

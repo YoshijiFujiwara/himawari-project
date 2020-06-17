6f059aa9c8e9967de54a031d8f75174b013d3d2b
tk-ozawa <ozawa.shotaro0114@gmail.com>
2020-06-17 16:31:34 +0900

    update: controllerとserviceに必要そうなパラメータ割り振り

--- api/src/commits/commits.controller.ts
+++ api/src/commits/commits.controller.ts
@@ -1,4 +1,38 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  UseGuards,
+  Body,
+  Param,
+  ParseIntPipe,
+} from '@nestjs/common';
+import { ApiCreatedResponse, ApiTags, ApiBearerAuth } from '@nestjs/swagger';
+import { CommitsService } from './commits.service';
+import { AuthGuard } from '@nestjs/passport';
+import { CreateCommitDto } from './dto/create-commit.dto';
+import { UserEntity } from '../auth/user.entity';
+import { GetUser } from '../auth/get-user-decorator';
 
-@Controller('commits')
-export class CommitsController {}
+@ApiTags('commits')
+@Controller('goals/:goal_id/commits')
+@UseGuards(AuthGuard('jwt'))
+@ApiBearerAuth()
+export class CommitsController {
+  constructor(private commitsService: CommitsService) {}
+
+  @Post()
+  @ApiCreatedResponse({
+    description: '学習記録の作成',
+  })
+  async createCommit(
+    @Body() createCommitDto: CreateCommitDto,
+    @Param('goal_id', ParseIntPipe) goal_id: number,
+    @GetUser() user: UserEntity,
+  ) {
+    return {
+      ...createCommitDto,
+      goal_id,
+      user,
+    };
+  }
+}
--- api/src/commits/commits.service.ts
+++ api/src/commits/commits.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { CommitRepository } from './commit.repository';
+import { CreateCommitDto } from './dto/create-commit.dto';
+import { GoalEntity } from '../goals/goal.entity';
 
 @Injectable()
-export class CommitsService {}
+export class CommitsService {
+  constructor(
+    @InjectRepository(CommitRepository)
+    private commitRepository: CommitRepository,
+  ) {}
+
+  async createCommit(createCommitDto: CreateCommitDto, goalId: number) {}
+}

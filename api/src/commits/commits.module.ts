7be9561c42395d379691b102b82dfe1877c2e260
tk-ozawa <ozawa.shotaro0114@gmail.com>
2020-06-17 12:53:45 +0900

    update: Commitsテーブル作成

--- api/src/commits/commits.module.ts
+++ api/src/commits/commits.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { CommitsService } from './commits.service';
 import { CommitsController } from './commits.controller';
+import { CommitRepository } from './commit.repository';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([CommitRepository]), AuthModule],
   providers: [CommitsService],
-  controllers: [CommitsController]
+  controllers: [CommitsController],
 })
 export class CommitsModule {}

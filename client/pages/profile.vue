0e7a1c6bcc72075c0c797343326b2916d3b6a64a
YoshijiFujiwara <yfbiology2@gmail.com>
2020-06-18 20:31:05 +0900

    delete dummy link

--- client/pages/profile.vue
+++ client/pages/profile.vue
@@ -3,11 +3,6 @@
     <h1>プロフィール</h1>
     <p>このページはログインしてないと見られないです！🌻</p>
     <h3>ユーザー情報: {{ JSON.stringify(Iam) }}</h3>
-    <ul>
-      <li><nuxt-link to="/goals/new">目標作成画面</nuxt-link></li>
-      <li><nuxt-link to="/goals/1">目標詳細画面</nuxt-link></li>
-    </ul>
-    <nuxt-link to="/auth/logout">ログアウト(仮)</nuxt-link>
   </div>
 </template>
 

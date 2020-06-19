1e1fced4ceffbdf15724de7876549393a728fc5f
kooririri <piaoxianglunb@gmail.com>
2020-06-19 23:58:14 +0900

    下の余白を解決

--- client/layouts/signin.vue
+++ client/layouts/signin.vue
@@ -41,6 +41,9 @@ export default Vue.extend({})
   min-height: 100vh; // 全体の高さを最低でもビューポートの100%にする
   display: flex; // 子クラスを横並びにする
   flex-direction: column; // 子クラスの横並びの方向を縦にする
+  .vs-row {
+    height: 100vh;
+  }
 }
 .main-image {
   background-color: #b6ddf2;

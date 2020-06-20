03a9a1efe267fc7dbfa3153047c408aa2119fce3
kooririri <piaoxianglunb@gmail.com>
2020-06-20 22:12:04 +0900

    signinの背景カラーと間隔調整

--- client/layouts/signin.vue
+++ client/layouts/signin.vue
@@ -40,13 +40,14 @@ export default Vue.extend({})
 .app {
   min-height: 100vh; // 全体の高さを最低でもビューポートの100%にする
   display: flex; // 子クラスを横並びにする
-  flex-direction: column; // 子クラスの横並びの方向を縦にする
+  // flex-direction: column; // 子クラスの横並びの方向を縦にする
   .vs-row {
-    height: 100vh;
+    // height: 100vh;
+    // min-height: 100%;
   }
 }
 .main-image {
-  background-color: #b6ddf2;
+  background-color: #ff896e;
   width: 100%;
   height: 100%;
   img {
--- client/layouts/signup.vue
+++ client/layouts/signup.vue
@@ -40,9 +40,9 @@ export default Vue.extend({})
 .app {
   min-height: 100vh; // 全体の高さを最低でもビューポートの100%にする
   display: flex; // 子クラスを横並びにする
-  flex-direction: column; // 子クラスの横並びの方向を縦にする
+  // flex-direction: column; // 子クラスの横並びの方向を縦にする
   .vs-row {
-    height: 100vh;
+    // height: 100vh;
   }
 }
 .main-image {

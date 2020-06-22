e53947015ebf1845cc1820a572670e9f27e642d2
libra219 <15705883+libra219@users.noreply.github.com>
2020-06-22 18:15:08 +0900

    ✨ : Add design group/new
    groups/newページのモック作成

---
+++ client/pages/groups/new.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <CreateGroupForm />
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+import CreateGroupForm from '@/components/organisms/groups/new/CreateGroupForm'
+
+export default Vue.extend({
+  middleware: 'authenticated',
+  components: {
+    CreateGroupForm
+  }
+})
+</script>
+
+<style></style>

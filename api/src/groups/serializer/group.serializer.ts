aca8368ded7291b242612398fee2078546f9c6f4
YoshijiFujiwara <yfbiology2@gmail.com>
2020-06-22 13:16:37 +0900

    fix test

--- api/src/groups/serializer/group.serializer.ts
+++ api/src/groups/serializer/group.serializer.ts
@@ -1,5 +1,5 @@
 import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
-import { UserSerializer } from '../auth/serializer/user.serializer';
+import { UserSerializer } from '../../auth/serializer/user.serializer';
 
 export class GroupSerializer {
   @ApiProperty()

4559aeef13b00f30e489c9b3f5ff85925095a84c
orphis <s_yufuyufu@icloud.com>
2020-06-22 18:08:54 +0900

    レイアウトが合うようにしてコンポーネントに分けた

---
+++ client/components/organisms/profile/CommitsTable.vue
@@ -0,0 +1,17 @@
+<template>
+  <vs-col vs-type="flex" vs-w="12">
+    <vs-card>
+      <vs-list v-for="(col, index) in 3" :key="index">
+        <vs-list-item title="ここtable"></vs-list-item>
+      </vs-list>
+    </vs-card>
+  </vs-col>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({})
+</script>
+
+<style lang="scss" scoped></style>
--- client/pages/profile.vue
+++ client/pages/profile.vue
@@ -1,21 +1,35 @@
 <template>
   <div>
-    <h1>プロフィール</h1>
     <vs-row>
-      <vs-col vs-type="flex" vs-justify="center" vs-align="center" vs-w="3">
-        2
+      <vs-col vs-type="flex" vs-justify="center" vs-align="center" vs-w="2">
+        <UserInfo />
       </vs-col>
-      <vs-col vs-type="flex" vs-justify="center" vs-align="center" vs-w="9">
-        10
-      </vs-col>
+      <vs-row vs-type="flex" vs-justify="center" vs-align="center" vs-w="10">
+        <GoalList />
+        <vs-col vs-type="flex" vs-justify="center" vs-align="center" vs-w="12">
+          <CommitsSummary></CommitsSummary>
+        </vs-col>
+        <CommitsTable />
+      </vs-row>
     </vs-row>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
+import CommitsSummary from '@/components/organisms/profile/CommitsSummary.vue'
+import CommitsTable from '@/components/organisms/profile/CommitsTable.vue'
+import GoalList from '@/components/organisms/profile/GoalList.vue'
+import UserInfo from '@/components/organisms/profile/UserInfo.vue'
 
 export default Vue.extend({
-  middleware: 'authenticated'
+  middleware: 'authenticated',
+  components: {
+    CommitsSummary,
+    CommitsTable,
+    GoalList,
+    UserInfo
+  }
 })
 </script>
+<style lang="scss" scoped></style>

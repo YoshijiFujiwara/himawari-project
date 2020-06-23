3fd9a6df54edfa5e184ffa432f46ae577f12337f
YoshijiFujiwara <yfbiology2@gmail.com>
2020-06-23 11:52:45 +0900

    dangerの明度を下げる

--- client/plugins/vuesax.ts
+++ client/plugins/vuesax.ts
@@ -7,7 +7,7 @@ Vue.use(Vuesax as any, {
     colors: {
       primary: '#1996fe',
       success: 'rgb(23, 201, 100)',
-      danger: 'rgb(242, 19, 93)',
+      danger: '#D00505',
       warning: 'rgb(255, 130, 0)',
       dark: 'rgb(36, 33, 69)'
     }

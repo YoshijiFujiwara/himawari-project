a4c899cb2b9ead01f7ab881f396ceb0803531f8c
Sui1224 <sui.ohs75003@gmail.com>
2020-06-25 00:25:08 +0900

    commitsTableコピペしただけなのでエラーでる明日やる

--- client/components/organisms/profile/CommitsTable.vue
+++ client/components/organisms/profile/CommitsTable.vue
@@ -1,17 +1,98 @@
 <template>
-  <vs-col vs-type="flex" vs-w="12">
-    <vs-card>
-      <vs-list v-for="(col, index) in 3" :key="index">
-        <vs-list-item title="ここtable"></vs-list-item>
-      </vs-list>
-    </vs-card>
-  </vs-col>
+  <vs-card>
+    <!-- https://lusaxweb.github.io/vuesax/components/card.html#media -->
+    <!-- を見ると、slot="media"で、カード内部のマージンがなくなるので使用している -->
+    <div slot="media">
+      <vs-table :data="commits">
+        <template slot="header">
+          <div class="table-header">
+            <h3>
+              yyyy年mm月dd日
+            </h3>
+          </div>
+        </template>
+        <template slot-scope="{ data }">
+          <vs-tr v-for="(tr, indextr) in data" :key="indextr" :data="tr">
+            <vs-td class="commit-td">
+              <vs-row vs-type="flex" vs-justify="space-between">
+                <vs-col
+                  vs-type="flex"
+                  vs-justify="center"
+                  vs-align="center"
+                  vs-w="2"
+                >
+                  <vs-icon icon="edit" color="primary"></vs-icon>
+                  <vs-col
+                    ><vs-row
+                      ><span class="commit-name">{{ tr.title }}</span></vs-row
+                    >
+                    <vs-row>{{
+                      `${tr.studyHours}時間${tr.studyMinutes}分`
+                    }}</vs-row></vs-col
+                  >
+                </vs-col>
+                <vs-col
+                  vs-type="flex"
+                  vs-justify="center"
+                  vs-align="center"
+                  vs-w="2"
+                >
+                  <vs-button
+                    type="flat"
+                    color="warning"
+                    icon="mood"
+                  ></vs-button>
+                  <vs-button
+                    type="flat"
+                    color="dark"
+                    icon="delete_outline"
+                  ></vs-button>
+                </vs-col>
+              </vs-row>
+            </vs-td>
+            <template slot="expand">
+              <vs-row>
+                <vs-col vs-offset="1" vs-type="flex">{{
+                  tr.description
+                }}</vs-col>
+              </vs-row>
+            </template>
+          </vs-tr>
+        </template>
+      </vs-table>
+    </div>
+  </vs-card>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
+import Vue, { PropType } from 'vue'
+import { CommitSerializer } from '@/openapi'
 
-export default Vue.extend({})
+export default Vue.extend({
+  props: {
+    commits: Array as PropType<CommitSerializer[]>
+  }
+})
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.table-header {
+  margin-top: 1rem;
+  margin-left: 1rem;
+  margin-bottom: 0.5rem;
+}
+.table-action-buttons {
+  display: flex;
+  justify-content: flex-end;
+}
+.commit-td {
+  width: 97%;
+}
+.commit-name {
+  font-weight: bold;
+}
+.commit-description {
+  display: flex;
+  justify-content: flex-start;
+}
+</style>

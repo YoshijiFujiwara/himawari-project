f6bcab4b55d2dad51e40fab037ba4a888b85d936
Sui1224 <sui.ohs75003@gmail.com>
2020-06-25 10:22:28 +0900

    data追加

--- client/components/organisms/profile/CommitsTable.vue
+++ client/components/organisms/profile/CommitsTable.vue
@@ -24,11 +24,9 @@
                   <vs-icon icon="edit" color="primary"></vs-icon>
                   <vs-col
                     ><vs-row
-                      ><span class="commit-name">{{ tr.title }}</span></vs-row
+                      ><span class="commit-name">{{ tr.name }}</span></vs-row
                     >
-                    <vs-row>{{
-                      `${tr.studyHours}時間${tr.studyMinutes}分`
-                    }}</vs-row></vs-col
+                    <vs-row>{{ `${tr.id}時間${tr.id}分` }}</vs-row></vs-col
                   >
                 </vs-col>
                 <vs-col
@@ -52,9 +50,7 @@
             </vs-td>
             <template slot="expand">
               <vs-row>
-                <vs-col vs-offset="1" vs-type="flex">{{
-                  tr.description
-                }}</vs-col>
+                <vs-col vs-offset="1" vs-type="flex">{{ tr.email }}</vs-col>
               </vs-row>
             </template>
           </vs-tr>
@@ -65,12 +61,84 @@
 </template>
 
 <script lang="ts">
-import Vue, { PropType } from 'vue'
-import { CommitSerializer } from '@/openapi'
+import Vue from 'vue'
 
 export default Vue.extend({
-  props: {
-    commits: Array as PropType<CommitSerializer[]>
+  data() {
+    return {
+      goals: [
+        {
+          id: 1,
+          name: 'Leanne Graham',
+          username: 'Bret',
+          email: 'Sincere@april.biz',
+          website: 'hildegard.org'
+        },
+        {
+          id: 2,
+          name: 'Ervin Howell',
+          username: 'Antonette',
+          email: 'Shanna@melissa.tv',
+          website: 'anastasia.net'
+        },
+        {
+          id: 3,
+          name: 'Clementine Bauch',
+          username: 'Samantha',
+          email: 'Nathan@yesenia.net',
+          website: 'ramiro.info'
+        },
+        {
+          id: 4,
+          name: 'Patricia Lebsack',
+          username: 'Karianne',
+          email: 'Julianne.OConner@kory.org',
+          website: 'kale.biz'
+        },
+        {
+          id: 5,
+          name: 'Chelsey Dietrich',
+          username: 'Kamren',
+          email: 'Lucio_Hettinger@annie.ca',
+          website: 'demarco.info'
+        },
+        {
+          id: 6,
+          name: 'Mrs. Dennis Schulist',
+          username: 'Leopoldo_Corkery',
+          email: 'Karley_Dach@jasper.info',
+          website: 'ola.org'
+        },
+        {
+          id: 7,
+          name: 'Kurtis Weissnat',
+          username: 'Elwyn.Skiles',
+          email: 'Telly.Hoeger@billy.biz',
+          website: 'elvis.io'
+        },
+        {
+          id: 8,
+          name: 'Nicholas Runolfsdottir V',
+          username: 'Maxime_Nienow',
+          email: 'Sherwood@rosamond.me',
+          website: 'jacynthe.com'
+        },
+        {
+          id: 9,
+          name: 'Glenna Reichert',
+          username: 'Delphine',
+          email: 'Chaim_McDermott@dana.io',
+          website: 'conrad.com'
+        },
+        {
+          id: 10,
+          name: 'Clementina DuBuque',
+          username: 'Moriah.Stanton',
+          email: 'Rey.Padberg@karina.biz',
+          website: 'ambrose.net'
+        }
+      ]
+    }
   }
 })
 </script>

6f7118af61500d29ab8808b40ce295b81f9f244e
Sui1224 <sui.ohs75003@gmail.com>
2020-06-25 10:48:10 +0900

    expand部分以外ある程度完成

--- client/components/organisms/profile/CommitsTable.vue
+++ client/components/organisms/profile/CommitsTable.vue
@@ -2,7 +2,7 @@
   <vs-card>
     <!-- https://lusaxweb.github.io/vuesax/components/card.html#media -->
     <!-- を見ると、slot="media"で、カード内部のマージンがなくなるので使用している -->
-    <div slot="media">
+    <div v-for="(col, index) in 3" slot="media" :key="index">
       <vs-table :data="commits">
         <template slot="header">
           <div class="table-header">
@@ -19,32 +19,14 @@
                   vs-type="flex"
                   vs-justify="center"
                   vs-align="center"
-                  vs-w="2"
+                  vs-w="3"
                 >
-                  <vs-icon icon="edit" color="primary"></vs-icon>
+                  <vs-icon :icon="tr.icon" color="primary"></vs-icon>
                   <vs-col
                     ><vs-row
-                      ><span class="commit-name">{{ tr.name }}</span></vs-row
+                      ><span class="commit-name">{{ tr.title }}</span></vs-row
                     >
-                    <vs-row>{{ `${tr.id}時間${tr.id}分` }}</vs-row></vs-col
-                  >
-                </vs-col>
-                <vs-col
-                  vs-type="flex"
-                  vs-justify="center"
-                  vs-align="center"
-                  vs-w="2"
-                >
-                  <vs-button
-                    type="flat"
-                    color="warning"
-                    icon="mood"
-                  ></vs-button>
-                  <vs-button
-                    type="flat"
-                    color="dark"
-                    icon="delete_outline"
-                  ></vs-button>
+                  </vs-col>
                 </vs-col>
               </vs-row>
             </vs-td>
@@ -66,76 +48,30 @@ import Vue from 'vue'
 export default Vue.extend({
   data() {
     return {
-      goals: [
+      commits: [
         {
           id: 1,
-          name: 'Leanne Graham',
+          title: '目標を1つ作成しました!',
           username: 'Bret',
+          icon: 'flag',
           email: 'Sincere@april.biz',
           website: 'hildegard.org'
         },
         {
           id: 2,
-          name: 'Ervin Howell',
+          title: '2つの目標に18回の学習を記録しました!',
           username: 'Antonette',
+          icon: 'edit',
           email: 'Shanna@melissa.tv',
           website: 'anastasia.net'
         },
         {
           id: 3,
-          name: 'Clementine Bauch',
+          title: '目標を1つ作成しました！',
           username: 'Samantha',
+          icon: 'flag',
           email: 'Nathan@yesenia.net',
           website: 'ramiro.info'
-        },
-        {
-          id: 4,
-          name: 'Patricia Lebsack',
-          username: 'Karianne',
-          email: 'Julianne.OConner@kory.org',
-          website: 'kale.biz'
-        },
-        {
-          id: 5,
-          name: 'Chelsey Dietrich',
-          username: 'Kamren',
-          email: 'Lucio_Hettinger@annie.ca',
-          website: 'demarco.info'
-        },
-        {
-          id: 6,
-          name: 'Mrs. Dennis Schulist',
-          username: 'Leopoldo_Corkery',
-          email: 'Karley_Dach@jasper.info',
-          website: 'ola.org'
-        },
-        {
-          id: 7,
-          name: 'Kurtis Weissnat',
-          username: 'Elwyn.Skiles',
-          email: 'Telly.Hoeger@billy.biz',
-          website: 'elvis.io'
-        },
-        {
-          id: 8,
-          name: 'Nicholas Runolfsdottir V',
-          username: 'Maxime_Nienow',
-          email: 'Sherwood@rosamond.me',
-          website: 'jacynthe.com'
-        },
-        {
-          id: 9,
-          name: 'Glenna Reichert',
-          username: 'Delphine',
-          email: 'Chaim_McDermott@dana.io',
-          website: 'conrad.com'
-        },
-        {
-          id: 10,
-          name: 'Clementina DuBuque',
-          username: 'Moriah.Stanton',
-          email: 'Rey.Padberg@karina.biz',
-          website: 'ambrose.net'
         }
       ]
     }

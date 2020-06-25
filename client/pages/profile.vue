2ce8d5fed894995f4b632b123e504666660cc347
YoshijiFujiwara <yfbiology2@gmail.com>
2020-06-26 01:52:31 +0900

    apiリクエスト下準備

--- client/pages/profile.vue
+++ client/pages/profile.vue
@@ -1,14 +1,24 @@
 <template>
   <div>
     <vs-row>
-      <vs-col vs-type="flex" vs-justify="center" vs-align="center" vs-w="2">
+      <vs-col vs-type="flex" vs-justify="center" vs-align="flex-start" vs-w="3">
         <UserInfo />
       </vs-col>
-      <vs-row vs-type="flex" vs-justify="center" vs-align="center" vs-w="10">
+      <vs-row vs-type="flex" vs-justify="flex-start" vs-align="center" vs-w="9">
+        <h2 class="section-title">目標一覧</h2>
         <GoalList />
-        <vs-col vs-type="flex" vs-justify="center" vs-align="center" vs-w="12">
-          <CommitsSummary />
+        <vs-col
+          vs-type="flex"
+          vs-justify="flex-start"
+          vs-align="center"
+          vs-w="12"
+        >
+          <div>
+            <h2 class="section-title">学習状況</h2>
+            <CommitsSummary />
+          </div>
         </vs-col>
+        <h2 class="section-title">学習記録</h2>
         <CommitsTable />
       </vs-row>
     </vs-row>
@@ -17,6 +27,7 @@
 
 <script lang="ts">
 import Vue from 'vue'
+import { goalStore, authStore } from '@/store'
 import CommitsSummary from '@/components/organisms/profile/CommitsSummary.vue'
 import CommitsTable from '@/components/organisms/profile/CommitsTable.vue'
 import GoalList from '@/components/organisms/profile/GoalList.vue'
@@ -29,7 +40,30 @@ export default Vue.extend({
     CommitsTable,
     GoalList,
     UserInfo
+  },
+  async created() {
+    this.$vs.loading()
+
+    // ログインユーザー情報の参照
+    await authStore.getMe()
+    // 目標の一覧
+    // await goalStore.getGoals()
+    // 自分の学習記録一覧取得（全ての目標を跨ぐ）
+    // await goalStore.getMyAllCommits()
+    // コミットのサマリーを取得（合計記録数や合計の時間）
+    // await goalStore.getCommitSummary()
+    // 月ごとのコミットの数を取得
+    // await goalStore.getCommitsByMonthly()
+
+    this.$vs.loading.close()
   }
 })
 </script>
-<style lang="scss" scoped></style>
+
+<style lang="scss" scoped>
+.section-title {
+  color: #54a9fe;
+  padding-left: 1rem;
+  margin-bottom: 1rem;
+}
+</style>

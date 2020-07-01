7f9e7278b5537be0a04eb635fa2758b55f1f50db
ke10310 <52442856+ke10310@users.noreply.github.com>
2020-07-01 13:03:43 +0900

    メール確認画面のレスポンシブ

--- client/pages/auth/email_confirmation.vue
+++ client/pages/auth/email_confirmation.vue
@@ -1,7 +1,33 @@
 <template>
-  <div>
-    メール確認が完了しました
-  </div>
+  <v-row class="mb-6 full-height" no-gutters>
+    <v-col cols="8">
+      <v-row>
+        <v-col class="ml-10 mt-10 mainText--text text-h4 font-weight-bold">
+          Project
+        </v-col>
+      </v-row>
+      <v-row justify="center" class="mt-10">
+        <v-col cols="12" md="8" class="text-center">
+          <v-img
+            class="mx-auto mb-5"
+            :src="require('@/assets/verify_complete.png')"
+            max-width="400"
+          />
+          <p class="text-h5 font-weight-bold">
+            アカウント登録を完了しました。
+          </p>
+          <p>
+            HimawariHubにご登録いただき、ありがとうございました。<br />
+            下記のボタンを押し、ログインすることができます。
+          </p>
+          <v-btn class="ma-2 mt-8" color="primary" to="/auth/signin">
+            ログインページへ
+          </v-btn>
+        </v-col>
+      </v-row>
+    </v-col>
+    <v-col v-show="_isPC" cols="12" md="4" class="yellowBg"></v-col>
+  </v-row>
 </template>
 
 <script lang="ts">
@@ -32,7 +58,7 @@ export default Vue.extend({
             type: 'warning'
           }))
         )
-        this.$router.push('/auth/signin')
+        // this.$router.push('/auth/signin')
       }
     } else {
       this._notifyyyy([
@@ -41,10 +67,15 @@ export default Vue.extend({
           type: 'warning'
         }
       ])
-      this.$router.push('/auth/signin')
+      // this.$router.push('/auth/signin')
     }
 
     this._finishLoading()
   }
 })
 </script>
+<style lang="scss" scoped>
+.full-height {
+  min-height: 100vh;
+}
+</style>

52bf0934b5ecf0d36822a5cbc8cb0c92e789e517
ke10310 <52442856+ke10310@users.noreply.github.com>
2020-07-01 12:39:51 +0900

    メール確認お願いページ

--- client/pages/auth/dont_email_confirmed.vue
+++ client/pages/auth/dont_email_confirmed.vue
@@ -1,13 +1,42 @@
 <template>
-  <div>
-    メールアドレスの確認をお願いします
-  </div>
+  <v-row class="mb-6 full-height" no-gutters>
+    <v-col cols="8">
+      <v-row>
+        <v-col class="ml-10 mt-10 mainText--text text-h4 font-weight-bold">
+          Project
+        </v-col>
+      </v-row>
+      <v-row justify="center" class="mt-10">
+        <v-col cols="9" class="text-center">
+          <v-img
+            class="mx-auto mb-5"
+            :src="require('@/assets/verify_ing.png')"
+            max-width="400"
+          />
+          <p class="text-h5 font-weight-bold">
+            メールアドレスの確認をお願いします。
+          </p>
+          <p>
+            HimawariHubを利用するには、メールアドレスの認証を完了する必要があります。<br />
+            <span class=" font-weight-bold">sumi@himawari.com</span
+            >に確認メールが送信されました。
+          </p>
+        </v-col>
+      </v-row>
+    </v-col>
+    <v-col cols="4" class="orangeBg"></v-col>
+  </v-row>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
-
 export default Vue.extend({
   layout: 'guest/default'
 })
 </script>
+
+<style lang="scss" scoped>
+.full-height {
+  min-height: 100vh;
+}
+</style>

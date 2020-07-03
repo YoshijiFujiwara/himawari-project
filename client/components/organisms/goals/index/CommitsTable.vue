9f47e4cf776a8bf10a15fbcc011c9508a193a66c
YoshijiFujiwara <yfbiology2@gmail.com>
2020-07-03 15:41:50 +0900

    hoge

--- client/components/organisms/goals/index/CommitsTable.vue
+++ client/components/organisms/goals/index/CommitsTable.vue
@@ -1,55 +1,50 @@
 <template>
   <div>
-    <v-card class="pa-0 mainText--text">
-      <template
-        v-for="dateStr in Object.keys(commitsByDay).sort((a, b) => a - b)"
-      >
-        <v-card-title :key="dateStr" class="commitTableHeaderBg pb-0">
-          <p>{{ dateStr }}</p>
-        </v-card-title>
-        <v-expansion-panels :key="dateStr" accordion multiple>
-          <v-expansion-panel
-            v-for="(commit, i) in commitsByDay[dateStr]"
-            :key="i"
-          >
-            <v-expansion-panel-header>
-              <div class="d-flex justify-space-between mainText--text">
-                <div class="d-flex align-self-center">
-                  <v-icon color="primary" class="mr-3">mdi-pencil</v-icon>
-                  <div class="d-flex flex-column">
-                    <p class="font-weight-bold text-subtitle-1 ma-0">
-                      {{ commit.title }}
-                    </p>
-                    <p>
-                      {{ `${commit.studyHours}時間${commit.studyMinutes}分` }}
-                    </p>
-                    <div>
-                      <v-chip small>
-                        <v-icon>mdi-emoticon-happy-outline</v-icon>
-                      </v-chip>
-                      <v-chip small>
-                        <v-icon>mdi-emoticon-kiss-outline</v-icon>
-                      </v-chip>
-                    </div>
+    <v-card v-for="(commit, i) in commits" :key="i" class="pa-0 mainText--text">
+      <v-card-title v-if="dateLabels[i]" class="commitTableHeaderBg pb-0">
+        <p>
+          {{ dateLables[i] }}
+        </p>
+      </v-card-title>
+      <v-expansion-panels accordion multiple>
+        <v-expansion-panel>
+          <v-expansion-panel-header>
+            <div class="d-flex justify-space-between mainText--text">
+              <div class="d-flex align-self-center">
+                <v-icon color="primary" class="mr-3">mdi-pencil</v-icon>
+                <div class="d-flex flex-column">
+                  <p class="font-weight-bold text-subtitle-1 ma-0">
+                    {{ commit.title }}
+                  </p>
+                  <p>
+                    {{ `${commit.studyHours}時間${commit.studyMinutes}分` }}
+                  </p>
+                  <div>
+                    <v-chip small>
+                      <v-icon>mdi-emoticon-happy-outline</v-icon>
+                    </v-chip>
+                    <v-chip small>
+                      <v-icon>mdi-emoticon-kiss-outline</v-icon>
+                    </v-chip>
                   </div>
                 </div>
-                <div class="d-flex align-self-center">
-                  <v-btn icon color="satisfyIcon">
-                    <v-icon>mdi-emoticon-outline</v-icon>
-                  </v-btn>
-                  <v-btn icon>
-                    <v-icon>mdi-delete-outline</v-icon>
-                  </v-btn>
-                </div>
               </div>
-            </v-expansion-panel-header>
-            <v-expansion-panel-content class="mainText--text">
-              {{ commit.description }}
-              {{ commit.createdAt }}
-            </v-expansion-panel-content>
-          </v-expansion-panel>
-        </v-expansion-panels>
-      </template>
+              <div class="d-flex align-self-center">
+                <v-btn icon color="satisfyIcon">
+                  <v-icon>mdi-emoticon-outline</v-icon>
+                </v-btn>
+                <v-btn icon>
+                  <v-icon>mdi-delete-outline</v-icon>
+                </v-btn>
+              </div>
+            </div>
+          </v-expansion-panel-header>
+          <v-expansion-panel-content class="mainText--text">
+            {{ commit.description }}
+            {{ commit.createdAt }}
+          </v-expansion-panel-content>
+        </v-expansion-panel>
+      </v-expansion-panels>
     </v-card>
     <div class="text-center">
       <v-pagination v-model="page" :length="6" circle></v-pagination>
@@ -59,7 +54,7 @@
 
 <script lang="ts">
 import Vue, { PropType } from 'vue'
-import { getDate, format } from 'date-fns'
+import { format } from 'date-fns'
 import { CommitSerializer } from '@/openapi'
 
 export default Vue.extend({
@@ -72,7 +67,6 @@ export default Vue.extend({
   data() {
     return {
       dateFns: {
-        getDate,
         format
       },
       page: 1
@@ -88,6 +82,19 @@ export default Vue.extend({
             acc[dateStr].push(commit)
           } else {
             acc[dateStr] = [commit]
+          }
+          return acc
+        },
+        {}
+      )
+    },
+    dateLabels(): { [key: number]: string } {
+      return this.commits.reduce(
+        (acc: { [key: number]: string }, commit, index) => {
+          const dateStr = format(new Date(commit.createdAt), 'yyyy年MM月dd日')
+          const found = Object.values(acc).find((d) => d === dateStr)
+          if (!found) {
+            acc[index] = dateStr
           }
           return acc
         },

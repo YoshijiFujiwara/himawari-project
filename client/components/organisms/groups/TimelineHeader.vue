27cee2fd01cd73687a45f31055408b720b7804ad
Sui1224 <sui.ohs75003@gmail.com>
2020-07-03 17:49:02 +0900

    ページコンポーネントの作成とtimeline用コンポーネント作成

---
+++ client/components/organisms/groups/TimelineHeader.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-row justify="center">
+    <v-col cols="12" md="8">
+      <p>たいむらいんへっだー</p>
+    </v-col>
+  </v-row>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  middleware: 'authenticated'
+})
+</script>

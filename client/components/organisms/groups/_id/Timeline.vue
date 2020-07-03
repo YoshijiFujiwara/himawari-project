49f8f43901ec5b424e4dafe1b2c9837425a1ead5
Sui1224 <sui.ohs75003@gmail.com>
2020-07-03 18:26:42 +0900

    タイムライン仮　作成

--- client/components/organisms/groups/_id/Timeline.vue
+++ client/components/organisms/groups/_id/Timeline.vue
@@ -1,7 +1,27 @@
 <template>
-  <v-row justify="center">
+  <v-row>
     <v-col cols="12" md="8">
-      <p>たいむらいん</p>
+      <v-timeline align-top>
+        <v-timeline-item v-for="n in 4" :key="n" right="true">
+          <template v-slot:icon>
+            <v-avatar>
+              <img src="http://i.pravatar.cc/64" />
+            </v-avatar>
+          </template>
+          <template v-slot:opposite>
+            <span>YYYY-MM-DD</span>
+          </template>
+          <v-row class="pt-1">
+            <v-col cols="3">
+              <strong>5pm</strong>
+            </v-col>
+            <v-col>
+              <strong>New Icon</strong>
+              <div class="caption">Mobile App</div>
+            </v-col>
+          </v-row>
+        </v-timeline-item>
+      </v-timeline>
     </v-col>
   </v-row>
 </template>

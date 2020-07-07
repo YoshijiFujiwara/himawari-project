21016b6467fb9920abf1c1efd05051530f3a6d46
libra219 <15705883+libra219@users.noreply.github.com>
2020-07-07 10:57:55 +0900

    🎨 : Add files
    プロフィール編集ダイアログのコンポーネント化

---
+++ client/components/organisms/profile/ProfileEditDialog.vue
@@ -0,0 +1,59 @@
+<template>
+  <v-card>
+    <v-card-title class="headline">
+      <v-icon color="primary">account-box</v-icon>
+      プロフィール編集
+    </v-card-title>
+    <v-divider></v-divider>
+    <v-card-text>
+      <v-form>
+        <v-row>
+          <v-col cols="4">
+            <v-img :src="require('@/assets/icon_sample.jpeg')" />
+            <v-file-input
+              label="File input"
+              filled
+              hide-input
+              prepend-icon="mdi-camera"
+            ></v-file-input>
+          </v-col>
+          <v-col cols="8">
+            <v-row>
+              <v-col cols="4">
+                <p>ユーザー名</p>
+                <v-text-field label="ユーザー名" outlined required>
+                </v-text-field>
+              </v-col>
+              <v-col cols="12">
+                <p>ステータスメッセージ</p>
+                <v-textarea label="ステータスメッセージ" outlined></v-textarea>
+              </v-col>
+            </v-row>
+          </v-col>
+        </v-row>
+        <v-divider></v-divider>
+        <v-row>
+          <v-col cols="12">
+            <v-btn large color="primary" @click="onSubmit">
+              プロフィールを保存する
+            </v-btn>
+          </v-col>
+        </v-row>
+      </v-form>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  data() {
+    return {
+      dialog: false
+    }
+  }
+})
+</script>
+
+<style></style>
--- client/components/organisms/profile/UserInfo.vue
+++ client/components/organisms/profile/UserInfo.vue
@@ -19,59 +19,7 @@
       プロフィール編集
     </v-btn>
     <v-dialog v-model="profileEditDialog" max-width="1000">
-      <!-- <ProfileEditDialog
-        :close-dialog="
-          () => {
-            profileEditDialog = false
-          }
-        "
-      /> -->
-      <v-card>
-        <v-card-title class="headline">
-          <v-icon color="primary">account-box</v-icon>
-          プロフィール編集
-        </v-card-title>
-        <v-divider></v-divider>
-        <v-card-text>
-          <v-form>
-            <v-row>
-              <v-col cols="4">
-                <v-img :src="require('@/assets/icon_sample.jpeg')" />
-                <v-file-input
-                  label="File input"
-                  filled
-                  hide-input
-                  prepend-icon="mdi-camera"
-                ></v-file-input>
-              </v-col>
-              <v-col cols="8">
-                <v-row>
-                  <v-col cols="4">
-                    <p>ユーザー名</p>
-                    <v-text-field label="ユーザー名" outlined required>
-                    </v-text-field>
-                  </v-col>
-                  <v-col cols="12">
-                    <p>ステータスメッセージ</p>
-                    <v-textarea
-                      label="ステータスメッセージ"
-                      outlined
-                    ></v-textarea>
-                  </v-col>
-                </v-row>
-              </v-col>
-            </v-row>
-            <v-divider></v-divider>
-            <v-row>
-              <v-col cols="12">
-                <v-btn large color="primary" @click="onSubmit">
-                  プロフィールを保存する
-                </v-btn>
-              </v-col>
-            </v-row>
-          </v-form>
-        </v-card-text>
-      </v-card>
+      <ProfileEditDialog />
     </v-dialog>
     <!-- PCのみ -->
     <v-divider v-show="_isPC" class="my-7"></v-divider>
@@ -139,11 +87,11 @@
 import Vue from 'vue'
 import { goalStore } from '@/store'
 import { CommitsSummary } from '@/openapi'
-// import ProfileEditDialog from '@/components/organisms/profile/ProfileEditDialog.vue'
+import ProfileEditDialog from '@/components/organisms/profile/ProfileEditDialog.vue'
 
 export default Vue.extend({
   components: {
-    // ProfileEditDialog
+    ProfileEditDialog
   },
   data() {
     return {

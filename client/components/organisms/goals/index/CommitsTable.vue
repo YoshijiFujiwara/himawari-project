e111e5ef8b371d01b041ace4a66190e8d2dfc93f
YoshijiFujiwara <35862303+YoshijiFujiwara@users.noreply.github.com>
2020-07-08 12:16:28 +0900

    Revert "削除ボタンを押した時のモーダルの表示"

--- client/components/organisms/goals/index/CommitsTable.vue
+++ client/components/organisms/goals/index/CommitsTable.vue
@@ -1,89 +1,149 @@
 <template>
-  <v-card class="pa-0 mainText--text">
-    <v-card-title class="commitTableHeaderBg pb-0">
-      <p>yyyy年mm月dd日</p>
-    </v-card-title>
-    <v-expansion-panels accordion multiple>
-      <v-expansion-panel v-for="(commit, i) in commits" :key="i">
-        <v-expansion-panel-header>
-          <div class="d-flex justify-space-between mainText--text">
-            <div class="d-flex align-self-center">
-              <v-icon color="primary" class="mr-3">mdi-pencil</v-icon>
-              <div class="d-flex flex-column">
-                <p class="font-weight-bold text-subtitle-1 ma-0">
-                  {{ commit.title }}
-                </p>
-                <p>{{ `${commit.studyHours}時間${commit.studyMinutes}分` }}</p>
-                <div>
-                  <v-chip small>
-                    <v-icon>mdi-emoticon-happy-outline</v-icon>
-                  </v-chip>
-                  <v-chip small>
-                    <v-icon>mdi-emoticon-kiss-outline</v-icon>
-                  </v-chip>
+  <div>
+    <v-card
+      v-for="(commit, i) in commitsPerPage"
+      :key="i"
+      class="pa-0 mainText--text rounded-0"
+    >
+      <v-card-title v-if="i in dateLabels" class="commitTableHeaderBg pb-0">
+        <p>
+          {{ dateLabels[i] }}
+        </p>
+      </v-card-title>
+      <v-expansion-panels accordion multiple>
+        <v-expansion-panel>
+          <v-expansion-panel-header>
+            <div class="d-flex justify-space-between mainText--text">
+              <div class="d-flex align-self-center">
+                <v-icon color="primary" class="mr-3">mdi-pencil</v-icon>
+                <div class="d-flex flex-column">
+                  <p class="font-weight-bold text-subtitle-1 ma-0">
+                    {{ commit.title }}
+                  </p>
+                  <p>
+                    {{ `${commit.studyHours}時間${commit.studyMinutes}分` }}
+                  </p>
+                  <div>
+                    <v-chip small>
+                      <v-icon>mdi-emoticon-happy-outline</v-icon>
+                    </v-chip>
+                    <v-chip small>
+                      <v-icon>mdi-emoticon-kiss-outline</v-icon>
+                    </v-chip>
+                  </div>
                 </div>
               </div>
+              <div class="d-flex align-self-center">
+                <v-btn icon color="satisfyIcon">
+                  <v-icon>mdi-emoticon-outline</v-icon>
+                </v-btn>
+                <v-btn icon>
+                  <v-icon>mdi-delete-outline</v-icon>
+                </v-btn>
+              </div>
             </div>
-            <div class="d-flex align-self-center">
-              <v-btn icon color="satisfyIcon">
-                <v-icon>mdi-emoticon-outline</v-icon>
-              </v-btn>
-              <v-btn icon @click="dialog = true">
-                <v-icon>mdi-delete-outline</v-icon>
-              </v-btn>
-            </div>
-          </div>
-        </v-expansion-panel-header>
-        <v-expansion-panel-content class="mainText--text">
-          {{ commit.description }}
-        </v-expansion-panel-content>
-      </v-expansion-panel>
-    </v-expansion-panels>
-    <v-dialog v-model="dialog" max-width="290">
-      <v-card>
-        <v-card-title class="headline">
-          学習記録を削除する
-        </v-card-title>
-
-        <v-card-text>
-          この学習記録を本当に削除しますか？削除後は元に戻すことはできません。
-        </v-card-text>
-
-        <v-card-actions>
-          <v-spacer></v-spacer>
-
-          <v-btn color="green darken-1" text @click="dialog = false">
-            Cancel
-          </v-btn>
-
-          <v-btn color="green darken-1" text @click="dialog = false">
-            OK
-          </v-btn>
-        </v-card-actions>
-      </v-card>
+          </v-expansion-panel-header>
+          <v-expansion-panel-content class="mainText--text">
+            {{ commit.description }}
+          </v-expansion-panel-content>
+        </v-expansion-panel>
+      </v-expansion-panels>
+    </v-card>
+    <div v-if="paginationLength" class="text-center mt-5">
+      <v-pagination
+        v-model="page"
+        :length="paginationLength"
+        circle
+      ></v-pagination>
+    </div>
+    <v-dialog v-model="isDialogOpen" max-width="600px">
+      <CreateCommitDialog
+        :close-dialog="
+          () => {
+            this.$emit('close')
+          }
+        "
+        :init-display-condition="initDisplayCondition"
+      />
     </v-dialog>
-  </v-card>
+  </div>
 </template>
 
 <script lang="ts">
 import Vue, { PropType } from 'vue'
+import { format } from 'date-fns'
 import { CommitSerializer } from '@/openapi'
+import CreateCommitDialog from '@/components/organisms/goals/index/CreateCommitDialog.vue'
 
 export default Vue.extend({
+  components: {
+    CreateCommitDialog
+  },
   props: {
     commits: {
       type: Array as PropType<CommitSerializer[]>,
+      required: true
+    },
+    createCommitDialog: {
+      type: Boolean,
       required: true
     }
   },
   data() {
     return {
-      dialog: false
+      dateFns: {
+        format
+      },
+      page: 1,
+      pageSize: 10
+    }
+  },
+  computed: {
+    commitsPerPage(): CommitSerializer[] {
+      return this.paginate(this.commits, this.pageSize, this.page)
+    },
+    dateLabels(): { [key: number]: string } {
+      return this.commitsPerPage.reduce(
+        (acc: { [key: number]: string }, commit, index) => {
+          const dateStr = format(new Date(commit.createdAt), 'yyyy年MM月dd日')
+          const found = Object.values(acc).find((d) => d === dateStr)
+          if (!found) {
+            acc[index] = dateStr
+          }
+          return acc
+        },
+        {}
+      )
+    },
+    paginationLength(): number {
+      return Math.ceil(this.commits.length / this.pageSize)
+    },
+    isDialogOpen: {
+      get() {
+        return this.createCommitDialog
+      },
+      set(isOpen) {
+        return isOpen
+      }
+    }
+  },
+  watch: {
+    createCommitDialog(newVal) {
+      if (!newVal) {
+        this.$emit('close')
+      }
     }
   },
   methods: {
-    hoge() {
-      alert('hoge')
+    paginate(
+      array: Array<any>,
+      pageSize: number,
+      pageNumber: number
+    ): Array<any> {
+      return array.slice((pageNumber - 1) * pageSize, pageNumber * pageSize)
+    },
+    initDisplayCondition() {
+      this.page = 1
     }
   }
 })

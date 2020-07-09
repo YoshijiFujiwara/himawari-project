f7271901683e8d7431193adb016f39607e272384
tk-ozawa <ozawa.shotaro0114@gmail.com>
2020-07-09 12:31:22 +0900

    add: timeline.moduleに各種必要なクラスをimport

--- api/src/timelines/timelines.module.ts
+++ api/src/timelines/timelines.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TimelinesController } from './timelines.controller';
 import { TimelinesService } from './timelines.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TimelineRepository } from './timeline.repository';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([TimelineRepository]), AuthModule],
   controllers: [TimelinesController],
   providers: [TimelinesService],
 })

09e6c1624300d6e4c9d9f424627c43a2e5c25974
ke10310 <52442856+ke10310@users.noreply.github.com>
2020-07-09 18:25:19 +0900

    デリートボタンを押した際のモーダル表示と学習記録のidの取得

--- client/components/organisms/goals/index/CommitsTable.vue
+++ client/components/organisms/goals/index/CommitsTable.vue
@@ -6,9 +6,7 @@
       class="pa-0 mainText--text rounded-0"
     >
       <v-card-title v-if="i in dateLabels" class="commitTableHeaderBg pb-0">
-        <p>
-          {{ dateLabels[i] }}
-        </p>
+        <p>{{ dateLabels[i] }}{{ deleteCommitId }}</p>
       </v-card-title>
       <v-expansion-panels accordion multiple>
         <v-expansion-panel>
@@ -37,7 +35,7 @@
                 <v-btn v-if="Iam.id != goal.userId" icon color="satisfyIcon">
                   <v-icon>mdi-emoticon-outline</v-icon>
                 </v-btn>
-                <v-btn icon>
+                <v-btn icon @click="openDeleteModal(commit.id)">
                   <v-icon>mdi-delete-outline</v-icon>
                 </v-btn>
               </div>
@@ -48,6 +46,29 @@
           </v-expansion-panel-content>
         </v-expansion-panel>
       </v-expansion-panels>
+      <v-dialog v-model="dialog" max-width="290">
+        <v-card>
+          <v-card-title class="headline">
+            学習記録を削除する
+          </v-card-title>
+
+          <v-card-text>
+            この学習記録を本当に削除しますか？削除後は元に戻すことはできません。
+          </v-card-text>
+
+          <v-card-actions>
+            <v-spacer></v-spacer>
+
+            <v-btn color="green darken-1" text @click="dialog = false">
+              キャンセル
+            </v-btn>
+
+            <v-btn color="error" text @click="deleteCommit">
+              削除する
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
     </v-card>
     <div v-if="paginationLength" class="text-center mt-5">
       <v-pagination
@@ -99,7 +120,9 @@ export default Vue.extend({
         format
       },
       page: 1,
-      pageSize: 10
+      pageSize: 10,
+      dialog: false,
+      deleteCommitId: null
     }
   },
   computed: {
@@ -148,6 +171,13 @@ export default Vue.extend({
     },
     initDisplayCondition() {
       this.page = 1
+    },
+    deleteCommit() {
+      alert('hogehoge')
+    },
+    openDeleteModal(commitId) {
+      this.dialog = true
+      this.deleteCommitId = commitId
     }
   }
 })

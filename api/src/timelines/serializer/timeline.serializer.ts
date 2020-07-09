0789599ef9a63981981fbe3500e4545869323f2a
tk-ozawa <ozawa.shotaro0114@gmail.com>
2020-07-09 10:03:48 +0900

    change: timeline.serializerを一旦commitのみを受け付けるようにした

--- api/src/timelines/serializer/timeline.serializer.ts
+++ api/src/timelines/serializer/timeline.serializer.ts
@@ -1,13 +1,12 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { CommitSerializer } from '../../commits/serializer/commit.serializer';
-import { GoalSerializer } from '../../goals/serializer/goal.serializer';
 
 export class TimelineSerializer {
   @ApiProperty()
   id: number;
 
   @ApiProperty({
-    type: CommitSerializer || GoalSerializer,
+    type: CommitSerializer,
   })
-  post: CommitSerializer | GoalSerializer;
+  commit: CommitSerializer;
 }

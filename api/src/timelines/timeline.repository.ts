3219fde96e4c9e47a5cbe10dc6d89695e9048ffa
tk-ozawa <ozawa.shotaro0114@gmail.com>
2020-07-09 10:04:45 +0900

    change: entity構造変更によるクエリ修正

--- api/src/timelines/timeline.repository.ts
+++ api/src/timelines/timeline.repository.ts
@@ -1,38 +1,23 @@
 import { EntityRepository, Repository } from 'typeorm';
 import { TimelineEntity } from './timeline.entity';
-import { TimelineReferenceType } from './timeline-reference-type.enum';
-import { GoalEntity } from '../goals/goal.entity';
+import { CommitEntity } from '../commits/commit.entity';
 
 @EntityRepository(TimelineEntity)
 export class TimelineRepository extends Repository<TimelineEntity> {
-  async sync(
-    goal: GoalEntity,
-    referenceId: number,
-    referenceType: TimelineReferenceType,
-  ): Promise<TimelineEntity> {
+  async sync(commit: CommitEntity): Promise<TimelineEntity> {
     const timeline = new TimelineEntity();
-    timeline.goal = goal;
-    timeline.referenceId = referenceId;
-    timeline.referenceType = referenceType;
-    await timeline.save();
+    timeline.commit = commit;
 
+    await timeline.save();
     return timeline;
   }
 
   async getByGroup(groupId: number) {
     return await this.createQueryBuilder('timeline')
-      .leftJoinAndSelect(
-        'timeline.commit',
-        'commit',
-        'timeline.reference_type = :referenceType',
-        { referenceType: TimelineReferenceType.COMMIT },
-      )
-      .leftJoinAndSelect(
-        'timeline.goal',
-        'goal',
-        'timeline.reference_type = :referenceType',
-        { referenceType: TimelineReferenceType.GOAL },
-      )
+      .leftJoinAndSelect('timeline.commit', 'commit')
+      .leftJoinAndSelect('commit.goal', 'goal')
+      .leftJoinAndSelect('goal.user', 'user')
+      .where('timeline.group_id = :groupId', { groupId })
       .getMany();
   }
 }

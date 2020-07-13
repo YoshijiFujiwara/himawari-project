02f043fdccd2dd5b87155b7916948d1622019ea3
tk-ozawa <ozawa.shotaro0114@gmail.com>
2020-07-13 12:35:20 +0900

    add: ダミーレスポンス用API作成

--- api/src/comments/comments.controller.ts
+++ api/src/comments/comments.controller.ts
@@ -1,4 +1,45 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  UseGuards,
+  Post,
+  Param,
+  ParseIntPipe,
+  ValidationPipe,
+  Body,
+} from '@nestjs/common';
+import { ApiTags, ApiBearerAuth, ApiCreatedResponse } from '@nestjs/swagger';
+import { AuthGuard } from '@nestjs/passport';
+import { CommentsService } from './comments.service';
+import { CommentSerializer } from './serializer/comment.serializer';
+import { CreateCommentDto } from './dto/create-comment.dto';
+import { GetUser } from '../auth/get-user-decorator';
+import { UserEntity } from '../auth/user.entity';
+@ApiTags('comments')
+@Controller()
+@UseGuards(AuthGuard('jwt'))
+@ApiBearerAuth()
+export class CommentsController {
+  constructor(private commentsService: CommentsService) {}
 
-@Controller('comments')
-export class CommentsController {}
+  @Post('timelines/:timeline_id/comments')
+  @ApiCreatedResponse({
+    description: 'コメントの投稿',
+    type: CommentSerializer,
+  })
+  async createComment(
+    @Param('timeline_id', ParseIntPipe) timelineId: number,
+    @Body(ValidationPipe) createCommentDto: CreateCommentDto,
+    @GetUser() user: UserEntity,
+  ): Promise<CommentSerializer> {
+    const commentSerializer = new CommentSerializer();
+    commentSerializer.id = 1;
+    commentSerializer.content = createCommentDto.content;
+    commentSerializer.userId = user.id;
+    commentSerializer.user = user.transformToSerializer();
+    commentSerializer.timelineId = timelineId;
+    commentSerializer.groupId = 1;
+    commentSerializer.createdAt = new Date();
+
+    return commentSerializer;
+  }
+}
--- api/src/comments/comments.service.ts
+++ api/src/comments/comments.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { CommentRepository } from './comment.repository';
 
 @Injectable()
-export class CommentsService {}
+export class CommentsService {
+  constructor(
+    @InjectRepository(CommentRepository)
+    private commentRepository: CommentRepository,
+  ) {}
+}

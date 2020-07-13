79852033379157a03215c2e8466a79787e22664a
tk-ozawa <ozawa.shotaro0114@gmail.com>
2020-07-13 23:58:50 +0900

    chore: codegen

--- client/openapi/api.ts
+++ client/openapi/api.ts
@@ -317,6 +317,12 @@ export interface GoalSerializer {
      * @type {string}
      * @memberof GoalSerializer
      */
+    lastCommitedAt: string;
+    /**
+     * 
+     * @type {string}
+     * @memberof GoalSerializer
+     */
     createdAt: string;
     /**
      * 

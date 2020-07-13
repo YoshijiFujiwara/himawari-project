6b7a8e29a0f57d1214adef9b84e550953c67bff5
tk-ozawa <ozawa.shotaro0114@gmail.com>
2020-07-13 20:45:26 +0900

    add: ダミーレスポンス作成

--- api/src/reactions/reactions.controller.ts
+++ api/src/reactions/reactions.controller.ts
@@ -1,4 +1,41 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  UseGuards,
+  Post,
+  Param,
+  ParseIntPipe,
+  Body,
+} from '@nestjs/common';
+import { ApiTags, ApiBearerAuth, ApiCreatedResponse } from '@nestjs/swagger';
+import { AuthGuard } from '@nestjs/passport';
+import { ReactionSerializer } from './serializer/reaction.serializer';
+import { CreateReactionDto } from './dto/create-reaction.dto';
+import { GetUser } from 'src/auth/get-user-decorator';
+import { UserEntity } from '../auth/user.entity';
+import { ReactionType } from './reaction-type.enum';
 
-@Controller('reactions')
-export class ReactionsController {}
+@ApiTags('timelines')
+@Controller('timelines/:id/reactions')
+@UseGuards(AuthGuard('jwt'))
+@ApiBearerAuth()
+export class ReactionsController {
+  @Post()
+  @ApiCreatedResponse({
+    description: 'リアクションの投稿',
+    type: ReactionSerializer,
+  })
+  async createReaction(
+    @Param('id', ParseIntPipe) timelineId: number,
+    @Body() createReactionDto: CreateReactionDto,
+    @GetUser() user: UserEntity,
+  ): Promise<ReactionSerializer> {
+    const reactionSerializer = new ReactionSerializer();
+    reactionSerializer.id = 1;
+    reactionSerializer.emoji = createReactionDto.emoji;
+    reactionSerializer.timelineId = timelineId;
+    reactionSerializer.userId = user.id;
+    reactionSerializer.user = user;
+
+    return reactionSerializer;
+  }
+}

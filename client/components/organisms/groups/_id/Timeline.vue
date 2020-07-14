3a4b9d96452c2ed6d2c11a6484dfd0efc96f3489
YoshijiFujiwara <yfbiology2@gmail.com>
2020-07-14 10:54:36 +0900

    creating comment menu

--- client/components/organisms/groups/_id/Timeline.vue
+++ client/components/organisms/groups/_id/Timeline.vue
@@ -48,39 +48,9 @@
                   <template v-slot:activator="{ on, attrs }">
                     <v-icon v-bind="attrs" v-on="on">mdi-reply</v-icon>
                   </template>
-
-                  <v-card>
-                    <v-list>
-                      <v-list-item>
-                        <v-list-item-content>
-                          <v-list-item-title class="text-subtitle2"
-                            >コメントする</v-list-item-title
-                          >
-                          <v-divider class="mt-3 mb-5"></v-divider>
-                          <v-textarea
-                            label="内容"
-                            outlined
-                            rows="3"
-                            clearable
-                            auto-grow
-                          ></v-textarea>
-                        </v-list-item-content>
-                      </v-list-item>
-                    </v-list>
-
-                    <v-card-actions class="mt-n10">
-                      <v-spacer></v-spacer>
-                      <v-btn text @click="commentMenu[timeline.id] = false"
-                        >閉じる</v-btn
-                      >
-                      <v-btn
-                        color="primary"
-                        depressed
-                        @click="commentMenu[timeline.id] = false"
-                        >送信</v-btn
-                      >
-                    </v-card-actions>
-                  </v-card>
+                  <CommentMenuCard
+                    :close-menu="closeCommentMenu(timeline.id)"
+                  />
                 </v-menu>
               </v-btn>
             </v-card-title>
@@ -107,10 +77,12 @@ import Vue from 'vue'
 import { groupStore } from '@/store'
 import { TimelineSerializer } from '@/openapi'
 import ReactionMenu from '@/components/organisms/groups/_id/ReactionMenu.vue'
+import CommentMenuCard from '@/components/organisms/groups/_id/CommentMenuCard.vue'
 
 export default Vue.extend({
   components: {
-    ReactionMenu
+    ReactionMenu,
+    CommentMenuCard
   },
   data() {
     return {
@@ -136,6 +108,13 @@ export default Vue.extend({
         this.commentMenu = menu
       }
       return groupStore.timelinesGetter
+    }
+  },
+  methods: {
+    closeCommentMenu(timelineId: number) {
+      return () => {
+        this.commentMenu[timelineId] = false
+      }
     }
   }
 })

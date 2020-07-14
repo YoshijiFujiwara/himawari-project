e2d831aaddc0fcf1ac7498a7f30ed9159120d1d3
Sui1224 <sui.ohs75003@gmail.com>
2020-07-14 13:52:27 +0900

    タイムライン形式に変更とボタン追加

--- client/components/organisms/groups/_id/GroupAssociation.vue
+++ client/components/organisms/groups/_id/GroupAssociation.vue
@@ -1,39 +1,54 @@
 <template>
   <div>
-    <v-row justify="start" no-gutter>
-      <v-col cols="10" offset="1">
-        <v-icon large color="black">
-          mdi-eye
-        </v-icon>
-        <span class="pt-3">
-          あなただけに表示されています。
-        </span>
-      </v-col>
-      <v-col cols="10" offset="1">
-        <p>
-          <v-avatar> <img src="@/assets/himawari.png"/></v-avatar>
-          <span class="font-weight-bold ml-4">ひまわり</span>
-        </p>
-        <p class="ml-16">
-          タイムライン画面へようこそ！<br />
-          ここでは記録した内容をグループにシェアしたり、シェアされた内容にコメントやリアクションすることができます！<br />
-          さあ、目標を登録してグループでのシェアを始めよう！
-        </p>
-      </v-col>
-
-      <v-col cols="9" offset="1">
-        <v-card>
-          <v-col v-for="n in 2" :key="n" cols="6">
-            <v-select
-              :items="goals"
-              item-text="title"
-              item-value="id"
-              :label="`設定する目標${n}`"
-              outlined
-              max-width="80"
-            ></v-select>
-          </v-col>
-        </v-card>
+    <v-row justify="start">
+      <v-col cols="10">
+        <div class="ml-16">
+          <v-icon large color="black">
+            mdi-eye
+          </v-icon>
+          <span class="pt-3">
+            あなただけに表示されています。
+          </span>
+        </div>
+        <v-timeline align-top dense>
+          <v-timeline-item>
+            <template v-slot:icon>
+              <v-avatar>
+                <img src="@/assets/himawari.png" />
+              </v-avatar>
+            </template>
+            <p class="font-weight-bold">ひまわり</p>
+            <p class="">
+              タイムライン画面へようこそ！<br />
+              ここでは記録した内容をグループにシェアしたり、シェアされた内容にコメントやリアクションすることができます！<br />
+              さあ、目標を登録してグループでのシェアを始めよう！
+            </p>
+            <v-card>
+              <v-row v-for="n in 2" :key="n" class="ml-4 pt-6">
+                <v-col cols="6">
+                  <v-select
+                    :items="goals"
+                    item-text="title"
+                    item-value="id"
+                    :label="`設定する目標${n}`"
+                    outlined
+                    max-width="80"
+                    clearable="true"
+                  ></v-select> </v-col
+                ><v-col>
+                  <v-btn height="50" width="50" outlined @click="clearGoal">
+                    <v-icon>mdi-window-close</v-icon>
+                  </v-btn>
+                </v-col>
+              </v-row>
+              <v-row class="ml-4">
+                <v-col>
+                  <v-btn color="primary" @click="onSubmit">保存する</v-btn>
+                </v-col>
+              </v-row>
+            </v-card>
+          </v-timeline-item>
+        </v-timeline>
       </v-col>
     </v-row>
   </div>
@@ -51,6 +66,14 @@ export default Vue.extend({
   computed: {
     goals(): GoalSerializer[] {
       return goalStore.goalsGetter
+    }
+  },
+  methods: {
+    clearGoal() {
+      alert('選択解除')
+    },
+    onSubmit() {
+      alert('保存しました')
     }
   }
 })

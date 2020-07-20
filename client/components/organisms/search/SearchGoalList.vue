fcea5e7fd324ecd2453c4a6819c7acb903211b2a
Sui1224 <sui.ohs75003@gmail.com>
2020-07-21 00:30:05 +0900

    目標検索微調整

--- client/components/organisms/search/SearchGoalList.vue
+++ client/components/organisms/search/SearchGoalList.vue
@@ -1,12 +1,9 @@
 <template>
   <div>
     <v-list v-for="n in 10" :key="n" class="elevation-1">
-      <v-list-item>
-        <v-list-item-icon>
-          <v-avatar color="indigo"> </v-avatar>
-        </v-list-item-icon>
+      <v-list-item class="ml-12">
         <v-list-item-content>
-          <v-list-item-title>UserName</v-list-item-title>
+          <v-list-item-title class="mainText--text">UserName</v-list-item-title>
           <v-list-item-subtitle class="mt-2">
             <v-icon>mdi-earth</v-icon
             ><span class="font-weight-bold mainText--text">目標名</span
@@ -15,10 +12,10 @@
               Challenging
             </v-chip>
           </v-list-item-subtitle>
-          <v-list-item-subtitle class="mainText--text">
+          <v-list-item-subtitle class="mainText--text ml-3">
             「目標について」の内容が表示されます。「目標について」の内容が表示されます。「目標について」の内容が表示されます．．．
           </v-list-item-subtitle>
-          <v-list-item-subtitle class="mt-2">
+          <v-list-item-subtitle class="mt-2 ml-3">
             <div>
               <span class="mr-3">
                 <v-icon color="primary">mdi-timer-outline</v-icon>99h99m

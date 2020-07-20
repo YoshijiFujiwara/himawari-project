d97ef9eb0e395cbe9a173d5ec11a437c96b127e8
YoshijiFujiwara <yfbiology2@gmail.com>
2020-07-20 13:37:35 +0900

    adding import

--- api/src/goals/goals.module.ts
+++ api/src/goals/goals.module.ts
@@ -4,9 +4,16 @@ import { GoalsController } from './goals.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { GoalRepository } from './goal.repository';
 import { AuthModule } from '../auth/auth.module';
+import { TimelineRepository } from '../timelines/timeline.repository';
+import { GroupRepository } from '../groups/group.repository';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([GoalRepository]), AuthModule],
+  imports: [
+    TypeOrmModule.forFeature([GoalRepository]),
+    TypeOrmModule.forFeature([TimelineRepository]),
+    TypeOrmModule.forFeature([GroupRepository]),
+    AuthModule,
+  ],
   providers: [GoalsService],
   controllers: [GoalsController],
 })

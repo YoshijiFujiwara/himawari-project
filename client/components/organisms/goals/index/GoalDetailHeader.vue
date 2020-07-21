3883e534757713e9d29c12214450bbf20baad103
hsmkyo <42708990+hsmkyo@users.noreply.github.com>
2020-07-21 16:22:09 +0900

    目標編集モック作成

--- client/components/organisms/goals/index/GoalDetailHeader.vue
+++ client/components/organisms/goals/index/GoalDetailHeader.vue
@@ -8,93 +8,12 @@
         {{ _isPC ? goal.title : '' }}
         <v-chip class="ma-2" color="chipBg">
           <v-icon left color="challengingColor">mdi-fire</v-icon>
-          Challenging
+          {{ goal.label }}
         </v-chip>
         <v-btn icon @click="goalEditDialog = true">
           <v-icon midium>mdi-cog</v-icon>
         </v-btn>
-        <v-dialog v-model="goalEditDialog" max-width="1000">
-          <GoalEditDialog />
-          <v-card>
-            <v-card-title>
-              <span class="headline">
-                <v-icon large>{{
-                  !!goal.isPublic ? 'mdi-earth' : 'mdi-lock-outline'
-                }}</v-icon>
-                {{ goal.title }}
-              </span>
-            </v-card-title>
-            <v-divider></v-divider>
-            <v-row justify="center" class="mt-5">
-              <v-col cols="10">
-                <v-form>
-                  <v-textarea label="目標について" outlined></v-textarea>
-                  <v-divider></v-divider>
-                  <p class="text-subtitle-2 mt-3 ml-3 font-weight-bold">
-                    ラベル
-                  </p>
-                  <v-radio-group v-model="radios" :mandatory="false">
-                    <v-radio
-                      class="text-h6 mb-3 font-weight-bold"
-                      label="Challenging"
-                      color="orange"
-                      value="Challenging"
-                    ></v-radio>
-                    <v-radio
-                      class="text-h6 mb-3 font-weight-bold"
-                      label="Achievement"
-                      color="green"
-                      value="Achievement"
-                    ></v-radio>
-                    <v-radio
-                      class="text-h6 mb-3 font-weight-bold"
-                      label="GiveUp"
-                      color="gray"
-                      value="GiveUp"
-                    ></v-radio>
-                  </v-radio-group>
-                  <v-divider></v-divider>
-                  <v-radio-group>
-                    <v-radio :value="true">
-                      <div slot="label" class="d-flex">
-                        <v-icon x-large class="mr-4">mdi-earth</v-icon>
-                        <div class="d-flex flex-column">
-                          <p class="text-h6 mb-0 font-weight-bold">公開</p>
-                          <p>
-                            誰でもこの目標を見ることができます。目標を公開にして、みんなと応援し合いましょう！
-                          </p>
-                        </div>
-                      </div>
-                    </v-radio>
-                    <v-radio :value="false">
-                      <div slot="label" class="d-flex">
-                        <v-icon x-large class="mr-4">mdi-lock-outline</v-icon>
-                        <div class="d-flex flex-column">
-                          <p class="text-h6 mb-0 font-weight-bold">非公開</p>
-                          <p>
-                            この目標はあなたもしくは、グループメンバーしか見ることができません。
-                          </p>
-                        </div>
-                      </div>
-                    </v-radio>
-                  </v-radio-group>
-                  <v-divider></v-divider>
-                  <v-col cols="12">
-                    <v-btn
-                      large
-                      color="primary"
-                      :disabled="!valid"
-                      :block="_isSP"
-                      @click="onSubmit"
-                    >
-                      目標を保存する
-                    </v-btn>
-                  </v-col>
-                </v-form>
-              </v-col>
-            </v-row>
-          </v-card>
-        </v-dialog>
+        <GoalEditDialog v-model="goalEditDialog" :goal="goal" />
       </p>
       <div class="d-flex justify-end">
         <p class="mr-4">
@@ -115,12 +34,12 @@
 <script lang="ts">
 import Vue, { PropType } from 'vue'
 import { GoalSerializer, CommitSerializer } from '@/openapi'
-// import GoalEditDialog from '@/components/organisms/goals/index/GoalEditDialog.vue'
+import GoalEditDialog from '@/components/organisms/goals/index/GoalEditDialog.vue'
 
 export default Vue.extend({
-  // components: {
-  //   GoalEditDialog
-  // },
+  components: {
+    GoalEditDialog
+  },
   props: {
     goal: {
       type: Object as PropType<GoalSerializer>,
@@ -159,3 +78,5 @@ export default Vue.extend({
   }
 })
 </script>
+
+<style></style>
--- client/components/organisms/goals/index/GoalEditDialog.vue
+++ client/components/organisms/goals/index/GoalEditDialog.vue
@@ -1,89 +1,123 @@
 <template>
-  <v-card>
-    <v-card-title>
-      <span class="headline">
-        <v-icon large>{{
-          !!goal.isPublic ? 'mdi-earth' : 'mdi-lock-outline'
-        }}</v-icon>
-        {{ goal.title }}
-      </span>
-    </v-card-title>
-    <v-divider></v-divider>
-    <v-row justify="center" class="mt-5">
-      <v-col cols="10">
-        <v-form>
-          <v-textarea label="目標について" outlined></v-textarea>
-          <v-divider></v-divider>
-          <p class="text-subtitle-2 mt-3 ml-3 font-weight-bold">
-            ラベル
-          </p>
-          <v-radio-group v-model="radios" :mandatory="false">
-            <v-radio
-              class="text-h6 mb-3 font-weight-bold"
-              label="Challenging"
-              color="orange"
-              value="Challenging"
-            ></v-radio>
-            <v-radio
-              class="text-h6 mb-3 font-weight-bold"
-              label="Achievement"
-              color="green"
-              value="Achievement"
-            ></v-radio>
-            <v-radio
-              class="text-h6 mb-3 font-weight-bold"
-              label="GiveUp"
-              color="gray"
-              value="GiveUp"
-            ></v-radio>
-          </v-radio-group>
-          <v-divider></v-divider>
-          <v-radio-group>
-            <v-radio :value="true">
-              <div slot="label" class="d-flex">
-                <v-icon x-large class="mr-4">mdi-earth</v-icon>
-                <div class="d-flex flex-column">
-                  <p class="text-h6 mb-0 font-weight-bold">公開</p>
-                  <p>
-                    誰でもこの目標を見ることができます。目標を公開にして、みんなと応援し合いましょう！
-                  </p>
+  <v-dialog v-model="dialog" max-width="1000">
+    <v-card>
+      <v-card-title>
+        <span class="headline">
+          <v-icon large>{{
+            !!goal.isPublic ? 'mdi-earth' : 'mdi-lock-outline'
+          }}</v-icon>
+          {{ goal.title }}
+        </span>
+      </v-card-title>
+      <v-divider></v-divider>
+      <v-row justify="center" class="mt-5">
+        <v-col cols="10">
+          <v-form>
+            <v-textarea
+              v-model="form.description"
+              label="目標について"
+              outlined
+            ></v-textarea>
+            <v-divider></v-divider>
+            <p class="text-subtitle-2 mt-5 ml-3 font-weight-bold">
+              ラベル
+            </p>
+            <v-radio-group v-model="form.status">
+              <v-radio
+                class="text-h6 mb-4 font-weight-bold"
+                label="Challenging"
+                color="orange"
+                value="CHALLENGING"
+              ></v-radio>
+              <v-radio
+                class="text-h6 mb-4 font-weight-bold"
+                label="Achievement"
+                color="green"
+                value="ACHIEVEMENT"
+              ></v-radio>
+              <v-radio
+                class="text-h6 mb-2 font-weight-bold"
+                label="GiveUp"
+                color="gray"
+                value="GIVEUP"
+              ></v-radio>
+            </v-radio-group>
+            <v-divider></v-divider>
+            <v-radio-group v-model="form.isPublic">
+              <v-radio :value="true">
+                <div slot="label" class="d-flex">
+                  <v-icon x-large class="mr-4">mdi-earth</v-icon>
+                  <div class="d-flex flex-column">
+                    <p class="text-h6 mb-0 font-weight-bold">公開</p>
+                    <p>
+                      誰でもこの目標を見ることができます。目標を公開にして、みんなと応援し合いましょう！
+                    </p>
+                  </div>
                 </div>
-              </div>
-            </v-radio>
-            <v-radio :value="false">
-              <div slot="label" class="d-flex">
-                <v-icon x-large class="mr-4">mdi-lock-outline</v-icon>
-                <div class="d-flex flex-column">
-                  <p class="text-h6 mb-0 font-weight-bold">非公開</p>
-                  <p>
-                    この目標はあなたもしくは、グループメンバーしか見ることができません。
-                  </p>
+              </v-radio>
+              <v-radio :value="false">
+                <div slot="label" class="d-flex">
+                  <v-icon x-large class="mr-4">mdi-lock-outline</v-icon>
+                  <div class="d-flex flex-column">
+                    <p class="text-h6 mb-0 font-weight-bold">非公開</p>
+                    <p>
+                      この目標はあなたもしくは、グループメンバーしか見ることができません。
+                    </p>
+                  </div>
                 </div>
-              </div>
-            </v-radio>
-          </v-radio-group>
-          <v-divider></v-divider>
-          <v-col cols="12">
-            <v-btn
-              large
-              color="primary"
-              :disabled="!valid"
-              :block="_isSP"
-              @click="onSubmit"
-            >
-              目標を保存する
-            </v-btn>
-          </v-col>
-        </v-form>
-      </v-col>
-    </v-row>
-  </v-card>
+              </v-radio>
+            </v-radio-group>
+            <v-divider></v-divider>
+            <v-col cols="12">
+              <v-btn large color="primary" :block="_isSP" @click="onSubmit">
+                目標を保存する
+              </v-btn>
+            </v-col>
+          </v-form>
+        </v-col>
+      </v-row>
+    </v-card>
+  </v-dialog>
 </template>
 
+<!-- いろいろ追加 -->
 <script lang="ts">
-import Vue from 'vue'
-
-export default Vue.extend({})
+import Vue, { PropType } from 'vue'
+import { GoalSerializer } from '@/openapi'
+export default Vue.extend({
+  data() {
+    return {
+      form: {
+        description: '',
+        status: '',
+        isPublic: false
+      }
+    }
+  },
+  created() {
+    this.form.description = this.goal.description || ''
+    this.form.status = this.goal.label
+    this.form.isPublic = this.goal.isPublic
+  },
+  props: {
+    value: {
+      type: Boolean,
+      required: true
+    },
+    goal: {
+      type: Object as PropType<GoalSerializer>,
+      required: true
+    }
+  },
+  computed: {
+    dialog: {
+      get(): boolean {
+        return this.value
+      },
+      set(dialog: boolean) {
+        this.$emit('input', dialog)
+      }
+    }
+  }
+})
 </script>
-
-<style></style>

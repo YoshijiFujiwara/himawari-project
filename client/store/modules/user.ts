11aaec706877d2cc498d5895cddae0d33e01a4de
YoshijiFujiwara <yfbiology2@gmail.com>
2020-07-24 16:40:48 +0900

    adding get user action

--- client/store/modules/user.ts
+++ client/store/modules/user.ts
@@ -1,12 +1,39 @@
-import { VuexModule, Module } from 'vuex-module-decorators'
-// import { buildApi } from '@/store/utils'
-// import { UsersApi } from '~/openapi'
+import { VuexModule, Module, Mutation, Action } from 'vuex-module-decorators'
+import { UserSerializer, UsersApi } from '~/openapi'
+import {
+  ActionAxiosResponse,
+  buildApi,
+  resError,
+  resSuccess
+} from '@/store/utils'
 
-// const userApi = () => buildApi(UsersApi)
+const userApi = () => buildApi(UsersApi)
 
 @Module({
   stateFactory: true,
   name: 'modules/user',
   namespaced: true
 })
-export default class User extends VuexModule {}
+export default class User extends VuexModule {
+  private user: UserSerializer | null = null
+
+  public get userGetter() {
+    return this.user
+  }
+
+  @Mutation
+  public SET_USER(user: UserSerializer) {
+    this.user = user
+  }
+
+  @Action
+  public async getUser(userId: number): Promise<ActionAxiosResponse> {
+    return await userApi()
+      .usersControllerGetUser(userId)
+      .then((res) => {
+        this.SET_USER(res.data)
+        return resSuccess(res)
+      })
+      .catch((e) => resError(e))
+  }
+}
